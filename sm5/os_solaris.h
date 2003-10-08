f794c70e5beeba364c3932b8d261453473b2a401
Bruce Allen <ballen4705@users.noreply.github.com>
2003-10-08 01:30:00 +0000

    Oops
    svn-path: trunk@1079

---
+++ sm5/os_solaris.h
@@ -0,0 +1,16 @@
+#ifndef _OS_SOLARIS_H_
+#define _OS_SOLARIS_H_
+
+#define OS_XXXX_H_CVSID "$Id: os_solaris.h,v 1.1 2003/10/08 01:30:00 ballen4705 Exp $\n"
+
+#include "config.h"
+#include "atacmds.h"
+#include "scsicmds.h"
+
+
+
+
+
+
+
+#endif /* _OS_SOLARIS_H_ */
