9f1716b2103b8b3ce66ce9c3e2cf19898bb08e3d
Bruce Allen <ballen4705@users.noreply.github.com>
2003-10-14 12:12:12 +0000

    Added os_generic.[hc] with some documentation as a guide to future ports.
    svn-path: trunk@1118

---
+++ sm5/os_generic.h
@@ -0,0 +1,35 @@
+/*
+ * os_generic.h
+ *
+ * Home page of code is: http://smartmontools.sourceforge.net
+ *
+ * Copyright (C) 2003 NAME HERE <smartmontools-support@lists.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * (for example COPYING); if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This code was originally developed as a Senior Thesis by Michael Cornwell
+ * at the Concurrent Systems Laboratory (now part of the Storage Systems
+ * Research Center), Jack Baskin School of Engineering, University of
+ * California, Santa Cruz. http://ssrc.soe.ucsc.edu/
+ *
+ */
+
+#ifndef OS_GENERIC_H_
+#define OS_GENERIC_H_
+
+#define OS_XXXX_H_CVSID "$Id: os_generic.h,v 1.1 2003/10/14 12:12:12 ballen4705 Exp $\n"
+
+// Additional material should start here.  Note: to keep the '-V' CVS
+// reporting option working as intended, you should only #include
+// system include files <something.h>.  Local #include files
+// <"something.h"> should be #included in os_solaris.c
+
+
+#endif /* OS_GENERIC_H_ */
