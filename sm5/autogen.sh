7d32b35206e739a2b19d06be9c0ee3a947f48ec5
Guido Guenther <agx@sigxcpu.org>
2003-12-15 17:29:32 +0000

    explain howto use autoreconf
    svn-path: trunk@1366

--- sm5/autogen.sh
+++ sm5/autogen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
-# $Id: autogen.sh,v 1.2 2003/10/02 10:07:44 ballen4705 Exp $
+# $Id: autogen.sh,v 1.3 2003/12/15 17:29:32 guidog Exp $
+#
+# Generate ./configure from config.in and Makefile.in from Makefile.am.
+# This also adds files like missing,depcomp,install-sh to the source
+# direcory. To update these files at a later date use:
+#	autoreconf -f -i -v
 
 aclocal
 autoheader
