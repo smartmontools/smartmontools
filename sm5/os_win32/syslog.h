98a79b7baa72d26ff1f64b12ad5f5736ed1ae8b6
Christian Franke <christian.franke@t-online.de>
2004-03-13 22:25:53 +0000

    Added event log support for Win32
    svn-path: trunk@1590

---
+++ sm5/os_win32/syslog.h
@@ -0,0 +1,57 @@
+/*
+ * os_win32/syslog.h
+ *
+ * Home page of code is: http://smartmontools.sourceforge.net
+ *
+ * Copyright (C) 2004 Christian Franke <smartmontools-support@lists.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * (for example COPYING); if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef SYSLOG_H
+#define SYSLOG_H
+
+#define SYSLOG_H_CVSID "$Id: syslog.h,v 1.1 2004/03/13 22:25:53 chrfranke Exp $\n"
+
+#include <stdarg.h>
+
+/* EVENTLOG_ERROR_TYPE: */
+#define LOG_EMERG       0
+#define LOG_ALERT       1
+#define LOG_CRIT        2
+#define LOG_ERR         3
+/* EVENTLOG_WARNING_TYPE: */
+#define LOG_WARNING     4
+/* EVENTLOG_INFORMATION_TYPE: */
+#define LOG_NOTICE      5
+#define LOG_INFO        6
+#define LOG_DEBUG       7
+
+/* Ignored: */
+#define LOG_DAEMON      ( 3<<3)
+#define LOG_LOCAL0      (16<<3)
+#define LOG_LOCAL1      (17<<3)
+#define LOG_LOCAL2      (18<<3)
+#define LOG_LOCAL3      (19<<3)
+#define LOG_LOCAL4      (20<<3)
+#define LOG_LOCAL5      (21<<3)
+#define LOG_LOCAL6      (22<<3)
+#define LOG_LOCAL7      (23<<3)
+
+#define LOG_PID         0x01
+
+void openlog(const char * ident, int option, int facility);
+
+void closelog();
+
+void vsyslog(int priority, const char * message, va_list args);
+
+#endif /* SYSLOG_H */
