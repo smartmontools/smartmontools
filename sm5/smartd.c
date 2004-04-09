9cdd98e6255aabb2133d5cdb6faeb6ea4236a52b
Bruce Allen <ballen4705@users.noreply.github.com>
2004-04-09 03:55:07 +0000

    Print format error (64 bit quantity)
    svn-path: trunk@1661

--- sm5/smartd.c
+++ sm5/smartd.c
@@ -98,7 +98,7 @@ int getdomainname(char *, int); /* no declaration in header files! */
 extern const char *atacmdnames_c_cvsid, *atacmds_c_cvsid, *ataprint_c_cvsid, *escalade_c_cvsid, 
                   *knowndrives_c_cvsid, *os_XXXX_c_cvsid, *scsicmds_c_cvsid, *utility_c_cvsid;
 
-static const char *filenameandversion="$Id: smartd.c,v 1.310 2004/04/09 03:30:06 ballen4705 Exp $";
+static const char *filenameandversion="$Id: smartd.c,v 1.311 2004/04/09 03:55:07 ballen4705 Exp $";
 #ifdef NEED_SOLARIS_ATA_CODE
 extern const char *os_solaris_ata_s_cvsid;
 #endif
@@ -109,7 +109,7 @@ extern const char *syslog_win32_c_cvsid;
 extern const char *int64_vc6_c_cvsid;
 #endif
 #endif
-const char *smartd_c_cvsid="$Id: smartd.c,v 1.310 2004/04/09 03:30:06 ballen4705 Exp $" 
+const char *smartd_c_cvsid="$Id: smartd.c,v 1.311 2004/04/09 03:55:07 ballen4705 Exp $" 
 ATACMDS_H_CVSID ATAPRINT_H_CVSID CONFIG_H_CVSID EXTERN_H_CVSID INT64_H_CVSID
 KNOWNDRIVES_H_CVSID SCSICMDS_H_CVSID SMARTD_H_CVSID
 #ifdef SYSLOG_H_CVSID
@@ -445,7 +445,7 @@ void Goodbye(void){
 
   // useful for debugging -- have we managed memory correctly?
   if (debugmode || (bytes && exitstatus!=EXIT_NOMEM))
-    PrintOut(LOG_INFO, "Memory still allocated for devices at exit is %lld bytes.\n", bytes);
+    PrintOut(LOG_INFO, "Memory still allocated for devices at exit is %"PRId64" bytes.\n", bytes);
 
   // if we are exiting because of a code bug, tell user
   if (exitstatus==EXIT_BADCODE || (bytes && exitstatus!=EXIT_NOMEM))
