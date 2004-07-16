dc44ff94f62cdc3a050bcd859d4cfe5bb48780d8
geoffk1 <authors@smartmontools.org>
2004-07-16 01:41:20 +0000

    Add basic support for Darwin.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@1800 4ea69e1a-61f1-4043-bf83-b5c94c648137

---
+++ sm5/os_darwin.h
@@ -0,0 +1,32 @@
+/*
+ * os_generic.h
+ *
+ * Home page of code is: http://smartmontools.sourceforge.net
+ *
+ * Copyright (C) 2004 Geoff Keating <geoffk@geoffk.org>
+ * Copyright (C) 2003-4 Bruce Allen <smartmontools-support@lists.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * (for example COPYING); if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This code was originally developed as a Senior Thesis by Michael Cornwell
+ * at the Concurrent Systems Laboratory (now part of the Storage Systems
+ * Research Center), Jack Baskin School of Engineering, University of
+ * California, Santa Cruz. http://ssrc.soe.ucsc.edu/
+ *
+ */
+
+#ifndef OS_DARWIN_H_
+#define OS_DARWIN_H_
+
+#define OS_XXXX_H_CVSID "$Id: os_darwin.h,v 1.1 2004/07/16 01:41:20 geoffk1 Exp $\n"
+
+// There isn't actually any content here yet.
+
+#endif /* OS_DARWIN_H_ */
