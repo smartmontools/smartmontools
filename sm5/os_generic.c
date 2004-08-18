bc74f9eedb81283c5c8a09f23ee602aafb5d0fc9
ballen4705 <dummy@smartmontools.org>
2004-08-18 21:27:18 +0000

    Fixed missing "}"
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@1882 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- sm5/os_generic.c
+++ sm5/os_generic.c
@@ -55,7 +55,7 @@
 #include "os_generic.h"
 
 // Needed by '-V' option (CVS versioning) of smartd/smartctl
-const char *os_XXXX_c_cvsid="$Id: os_generic.c,v 1.16 2004/08/18 19:27:44 likewise Exp $" \
+const char *os_XXXX_c_cvsid="$Id: os_generic.c,v 1.17 2004/08/18 21:27:18 ballen4705 Exp $" \
 ATACMDS_H_CVSID OS_XXXX_H_CVSID SCSICMDS_H_CVSID UTILITY_H_CVSID;
 
 
@@ -187,6 +187,7 @@ int deviceclose(int fd){
 int ata_command_interface(int fd, smart_command_set command, int select, char *data){
   unsupported();
   return -1;
+}
 
 int marvell_command_interface(int fd, smart_command_set command, int select, char *data){
   unsupported();
