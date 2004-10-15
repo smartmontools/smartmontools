2d82be8c583f809a79e5293a2ff63e1b307cef1d
chrfranke <authors@smartmontools.org>
2004-10-15 07:57:22 +0000

    Windows: Added OS flavor info also to smartd
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@2007 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- sm5/smartd.cpp
+++ sm5/smartd.cpp
@@ -108,14 +108,14 @@ int getdomainname(char *, int); /* no declaration in header files! */
 extern const char *atacmdnames_c_cvsid, *atacmds_c_cvsid, *ataprint_c_cvsid, *escalade_c_cvsid, 
                   *knowndrives_c_cvsid, *os_XXXX_c_cvsid, *scsicmds_c_cvsid, *utility_c_cvsid;
 
-static const char *filenameandversion="$Id: smartd.cpp,v 1.344 2004/09/15 10:28:30 chrfranke Exp $";
+static const char *filenameandversion="$Id: smartd.cpp,v 1.345 2004/10/15 07:57:22 chrfranke Exp $";
 #ifdef NEED_SOLARIS_ATA_CODE
 extern const char *os_solaris_ata_s_cvsid;
 #endif
 #ifdef _WIN32
 extern const char *daemon_win32_c_cvsid, *hostname_win32_c_cvsid, *syslog_win32_c_cvsid;
 #endif
-const char *smartd_c_cvsid="$Id: smartd.cpp,v 1.344 2004/09/15 10:28:30 chrfranke Exp $" 
+const char *smartd_c_cvsid="$Id: smartd.cpp,v 1.345 2004/10/15 07:57:22 chrfranke Exp $" 
 ATACMDS_H_CVSID ATAPRINT_H_CVSID CONFIG_H_CVSID
 #ifdef DAEMON_WIN32_H_CVSID
 DAEMON_WIN32_H_CVSID
@@ -1038,7 +1038,12 @@ void WritePidFile() {
 
 // Prints header identifying version of code and home
 void PrintHead(){
-  PrintOut(LOG_INFO,"smartd version %s [%s] Copyright (C) 2002-4 Bruce Allen\n", PACKAGE_VERSION, SMARTMONTOOLS_BUILD_HOST);
+#ifdef HAVE_GET_OS_VERSION_STR
+  const char * ver = get_os_version_str();
+#else
+  const char * ver = SMARTMONTOOLS_BUILD_HOST;
+#endif
+  PrintOut(LOG_INFO,"smartd version %s [%s] Copyright (C) 2002-4 Bruce Allen\n", PACKAGE_VERSION, ver);
   PrintOut(LOG_INFO,"Home page is " PACKAGE_HOMEPAGE "\n\n");
   return;
 }
