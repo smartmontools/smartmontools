f3d9cbd9bd1c2c0f3920a29dc020bbc86d0cd614
Bruce Allen <ballen4705@users.noreply.github.com>
2008-02-23 06:41:59 +0000

    Fix Auto-offline support in FreeBSD.  Thanks to Cyrus Rahman <crahman@gmail.com> for the patch, and Eduard Martinescu for blessing it. ----------------------------------------------------------------------
    svn-path: trunk@2460

--- sm5/os_freebsd.cpp
+++ sm5/os_freebsd.cpp
@@ -44,9 +44,9 @@
 #include "extern.h"
 #include "os_freebsd.h"
 
-static const char *filenameandversion="$Id: os_freebsd.cpp,v 1.55 2007/12/08 17:11:11 shattered Exp $";
+static const char *filenameandversion="$Id: os_freebsd.cpp,v 1.56 2008/02/23 06:41:59 ballen4705 Exp $";
 
-const char *os_XXXX_c_cvsid="$Id: os_freebsd.cpp,v 1.55 2007/12/08 17:11:11 shattered Exp $" \
+const char *os_XXXX_c_cvsid="$Id: os_freebsd.cpp,v 1.56 2008/02/23 06:41:59 ballen4705 Exp $" \
 ATACMDS_H_CVSID CONFIG_H_CVSID INT64_H_CVSID OS_FREEBSD_H_CVSID SCSICMDS_H_CVSID UTILITY_H_CVSID;
 
 // to hold onto exit code for atexit routine
@@ -368,7 +368,8 @@ int ata_command_interface(int fd, smart_command_set command, int select, char *d
   case AUTO_OFFLINE:
     // NOTE: According to ATAPI 4 and UP, this command is obsolete
     request.u.ata.feature=ATA_SMART_AUTO_OFFLINE;
-    request.u.ata.lba=select|(0xc24f<<8);
+    request.u.ata.lba=0xc24f<<8;                                                                                                                                         
+    request.u.ata.count=select;                                                                                                                                          
     request.flags=ATA_CMD_CONTROL;
     break;
   case AUTOSAVE:
