a0cd3276ab90b904a31ac53415d2923698ec8776
Bruce Allen <ballen4705@users.noreply.github.com>
2008-03-15 12:04:29 +0000

    [BA] Fix ugly syntax bug in os_freebsd.cpp.  How did this go undetected for so long??
    svn-path: trunk@2480

--- sm5/os_freebsd.cpp
+++ sm5/os_freebsd.cpp
@@ -44,9 +44,9 @@
 #include "extern.h"
 #include "os_freebsd.h"
 
-static const char *filenameandversion="$Id: os_freebsd.cpp,v 1.58 2008/03/04 22:09:47 ballen4705 Exp $";
+static const char *filenameandversion="$Id: os_freebsd.cpp,v 1.59 2008/03/15 12:04:29 ballen4705 Exp $";
 
-const char *os_XXXX_c_cvsid="$Id: os_freebsd.cpp,v 1.58 2008/03/04 22:09:47 ballen4705 Exp $" \
+const char *os_XXXX_c_cvsid="$Id: os_freebsd.cpp,v 1.59 2008/03/15 12:04:29 ballen4705 Exp $" \
 ATACMDS_H_CVSID CONFIG_H_CVSID INT64_H_CVSID OS_FREEBSD_H_CVSID SCSICMDS_H_CVSID UTILITY_H_CVSID;
 
 // to hold onto exit code for atexit routine
@@ -272,7 +272,6 @@ int marvell_command_interface(__unused int fd, __unused smart_command_set comman
 }
 
 int highpoint_command_interface(__unused int fd, __unused smart_command_set command, __unused int select, __unused char *data) {
-{
   return -1;
 }
 
