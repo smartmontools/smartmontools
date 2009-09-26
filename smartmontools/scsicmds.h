a3865c61105995815cf076015742521d8ab6cca9
chrfranke <authors@smartmontools.org>
2009-09-26 20:38:40 +0000

    Increase SCSI_TIMEOUT_DEFAULT from 6 to 20 seconds to avoid timeouts when a disk spins up from standby mode.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@2924 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/scsicmds.h
+++ smartmontools/scsicmds.h
@@ -32,7 +32,7 @@
 #ifndef SCSICMDS_H_
 #define SCSICMDS_H_
 
-#define SCSICMDS_H_CVSID "$Id: scsicmds.h,v 1.69 2009/06/24 04:10:10 dpgilbert Exp $\n"
+#define SCSICMDS_H_CVSID "$Id$\n"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -273,7 +273,8 @@ Documentation, see http://www.storage.ibm.com/techsup/hddtech/prodspecs.htm */
 
 
 /* SCSI command timeout values (units are seconds) */
-#define SCSI_TIMEOUT_DEFAULT    6               /* 6 seconds should be ample */
+#define SCSI_TIMEOUT_DEFAULT    20  // should be longer than the spin up time
+                                    // of a disk in standby mode.
 #define SCSI_TIMEOUT_SELF_TEST  (5 * 60 * 60)   /* allow max 5 hours for */
                                             /* extended foreground self test */
 
