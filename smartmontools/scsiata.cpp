090e244b177a2e6099c190003f13b3c344962dbb
samm-git <samm@os2.kiev.ua>
2009-11-29 16:21:07 +0000

    Added USB IDs of WD Passport USB Portable
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@2988 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/scsiata.cpp
+++ smartmontools/scsiata.cpp
@@ -1373,6 +1373,7 @@ const usb_id_entry usb_ids[] = {
   { 0x0d49, 0x7410, 0x0122, d_sat     }, // Maxtor Basics Desktop
   { 0x0d49, 0x7450, 0x0122, d_sat     }, // Maxtor Basics Portable
   // Western Digital
+  { 0x1058, 0x0702, 0x0104, d_sat     }, // WD My Passport Portable  
   { 0x1058, 0x0704, 0x0175, d_sat     }, // WD My Passport Essential
   { 0x1058, 0x0705, 0x0175, d_sat     }, // WD My Passport Elite
   { 0x1058, 0x070a, 0x1028, d_sat     }, // WD My Passport 070A
