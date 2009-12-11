caccd4ddf17dc4a0333214fcca3a7a235dc8ac04
chrfranke <authors@smartmontools.org>
2009-12-11 22:51:04 +0000

    Move drive database entries from 'knowndrives.cpp' to new file 'drivedb.h'.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@2998 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/knowndrives.h
+++ smartmontools/knowndrives.h
@@ -4,8 +4,8 @@
  * Home page of code is: http://smartmontools.sourceforge.net
  * Address of support mailing list: smartmontools-support@lists.sourceforge.net
  *
- * Copyright (C) 2003-8 Philip Williams, Bruce Allen
- * Copyright (C) 2008   Christian Franke <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 2003-9 Philip Williams, Bruce Allen
+ * Copyright (C) 2008-9 Christian Franke <smartmontools-support@lists.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,25 +23,7 @@
 
 #define KNOWNDRIVES_H_CVSID "$Id$\n"
 
-/* Structure used to store settings for specific drives in knowndrives[]. The
- * elements are used in the following ways:
- *
- *  modelfamily     Informal string about the model family/series of a 
- *                  device. Set to "" if no info (apart from device id)
- *                  known.
- *  modelregexp     POSIX regular expression to match the model of a device.
- *                  This should never be "".
- *  firmwareregexp  POSIX regular expression to match a devices's firmware
- *                  version.  This is optional and should be "" if it is not
- *                  to be used.  If it is nonempty then it will be used to
- *                  narrow the set of devices matched by modelregexp.
- *  warningmsg      A message that may be displayed for matching drives.  For
- *                  example, to inform the user that they may need to apply a
- *                  firmware patch.
- *  presets         String with vendor-specific attribute ('-v') and firmware
- *                  bug fix ('-F') options.  Same syntax as in smartctl command
- *                  line.  The user's own settings override these.
- */
+// Structure to store drive database entries, see drivedb.h for a description.
 struct drive_settings {
   const char * modelfamily;
   const char * modelregexp;
