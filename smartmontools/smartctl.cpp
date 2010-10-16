3ccf2627059baf5b9372a99b8e7b5faddbe6afe8
chrfranke <authors@smartmontools.org>
2010-10-16 13:09:11 +0000

    smartd: Fix setting of SMARTD_DEVICE and SMARTD_DEVICETYPE environment variables (ticket #113).
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3186 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/smartctl.cpp
+++ smartmontools/smartctl.cpp
@@ -176,7 +176,7 @@ static std::string getvalidarglist(char opt)
   case 'q':
     return "errorsonly, silent, noserial";
   case 'd':
-    return smi()->get_valid_dev_types_str() + ", test";
+    return smi()->get_valid_dev_types_str() + ", auto, test";
   case 'T':
     return "normal, conservative, permissive, verypermissive";
   case 'b':
@@ -312,7 +312,7 @@ const char * parse_options(int argc, char** argv,
       }
       break;
     case 'd':
-      type = optarg;
+      type = (strcmp(optarg, "auto") ? optarg : (char *)0);
       break;
     case 'T':
       if (!strcmp(optarg,"normal")) {
