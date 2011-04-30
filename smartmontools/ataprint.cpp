646ac2ec14008947085d1d703052a0a3a4b6e4dc
Christian Franke <christian.franke@t-online.de>
2011-04-30 19:38:55 +0000

    smartctl: Always print sector size in '-i' output (ticket #166).
    svn-path: trunk@3318

--- smartmontools/ataprint.cpp
+++ smartmontools/ataprint.cpp
@@ -40,7 +40,7 @@
 #include "utility.h"
 #include "knowndrives.h"
 
-const char * ataprint_cpp_cvsid = "$Id: ataprint.cpp 3316 2011-04-19 19:34:57Z chrfranke $"
+const char * ataprint_cpp_cvsid = "$Id: ataprint.cpp 3318 2011-04-30 19:38:55Z chrfranke $"
                                   ATAPRINT_H_CVSID;
 
 
@@ -449,13 +449,8 @@ static void print_drive_info(const ata_identify_device * drive,
       format_capacity(cap, sizeof(cap), sizes.capacity));
 
     // Print sector sizes.
-    if (sizes.phy_sector_size == sizes.log_sector_size) {
-      // Don't print if drive reports the default values.
-      // Some 4KiB LPS drives report 512 bytes in IDENTIFY word 106
-      // (e.g. Samsung HD204UI).
-      if (sizes.log_sector_size != 512)
-        pout("Sector Size:      %u bytes logical/physical\n", sizes.log_sector_size);
-    }
+    if (sizes.phy_sector_size == sizes.log_sector_size)
+      pout("Sector Size:      %u bytes logical/physical\n", sizes.log_sector_size);
     else {
       pout("Sector Sizes:     %u bytes logical, %u bytes physical",
          sizes.log_sector_size, sizes.phy_sector_size);
