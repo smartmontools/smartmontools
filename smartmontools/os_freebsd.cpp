9f9d06a6dd0921aaccca6529a8e9fc012f1132b6
samm-git <samm@os2.kiev.ua>
2011-06-16 21:58:58 +0000

    os_freebsd.cpp: enable 48bit support for the new atacam interface
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3381 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_freebsd.cpp
+++ smartmontools/os_freebsd.cpp
@@ -258,10 +258,14 @@ int freebsd_ata_device::do_cmd( struct ata_ioc_request* request)
 
 bool freebsd_ata_device::ata_pass_through(const ata_cmd_in & in, ata_cmd_out & out)
 {
+  bool ata_48bit = false; // no ata_48bit_support via IOCATAREQUEST
+  if(!strcmp("atacam",get_dev_type())) // enable for atacam interface
+    ata_48bit = true;
+
   if (!ata_cmd_is_ok(in,
     true,  // data_out_support
     true,  // multi_sector_support
-    false) // no ata_48bit_support via IOCATAREQUEST
+    ata_48bit) 
     ) 
     return false;
 
