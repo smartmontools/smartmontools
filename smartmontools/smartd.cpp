1db4930d0762e1f1dd4af713798fcd684abef641
dpgilbert <authors@smartmontools.org>
2011-11-17 22:40:20 +0000

    [SCSI] smartd: skip non-storage devices (e.g. SES devices)
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3479 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/smartd.cpp
+++ smartmontools/smartd.cpp
@@ -2155,10 +2155,18 @@ static int SCSIDeviceScan(dev_config & cfg, dev_state & state, scsi_device * scs
   version = inqBuf[2];
   avail_len = inqBuf[4] + 5;
   len = (avail_len < req_len) ? avail_len : req_len;
-  // peri_dt = inqBuf[0] & 0x1f;
   if (len < 36) {
     PrintOut(LOG_INFO, "Device: %s, INQUIRY response less than 36 bytes; "
 	     "skip device\n", device);
+    return 2;
+  }
+
+  int pdt = inqBuf[0] & 0x1f;
+
+  if (! ((0 == pdt) || (4 == pdt) || (5 == pdt) || (7 == pdt) ||
+         (0xe == pdt))) {
+    PrintOut(LOG_INFO, "Device: %s, not a disk like device [PDT=0x%x], "
+             "skip\n", device, pdt);
     return 2;
   }
   lu_id[0] = '\0';
