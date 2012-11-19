b41e0f1972c3418a71e857a5d4e5663fae96ed71
samm-git <samm@os2.kiev.ua>
2012-11-19 21:39:38 +0000

    os_linux.cpp: add autodetection for PERC H700 array
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3698 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_linux.cpp
+++ smartmontools/os_linux.cpp
@@ -2316,7 +2316,9 @@ smart_device * linux_scsi_device::autodetect_open()
     }
 
     // DELL?
-    if (!memcmp(req_buff + 8, "DELL    PERC", 12) || !memcmp(req_buff + 8, "MegaRAID", 8)) {
+    if (!memcmp(req_buff + 8, "DELL    PERC", 12) || !memcmp(req_buff + 8, "MegaRAID", 8)
+        || !memcmp(req_buff + 16, "PERC H700", 9)
+    ) {
       close();
       set_err(EINVAL, "DELL or MegaRaid controller, please try adding '-d megaraid,N'");
       return this;
