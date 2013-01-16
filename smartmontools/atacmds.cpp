smartmontools/smartmontools

commit fb692ef52254c70c73411c35ed9d76d78b21dac2

Avoid usage of strcpy(), strcat(), sprintf(). Use snprintf() instead or change type to std::string. Use array references instead of char pointers for parameters.
git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3719 4ea69e1a-61f1-4043-bf83-b5c94c648137

smartmontools/atacmds.cpp
@@ -229,7 +229,7 @@ bool parse_attribute_def(const char * opt, ata_vendor_attr_defs & defs,
       defs[i].priority = priority;
       defs[i].raw_format = format;
       defs[i].flags = flags;
-      strcpy(defs[i].byteorder, byteorder);
+      snprintf(defs[i].byteorder, sizeof(defs[i].byteorder), "%s", byteorder);
     }
   }
   else if (defs[id].priority <= priority) {
@@ -239,7 +239,7 @@ bool parse_attribute_def(const char * opt, ata_vendor_attr_defs & defs,
     defs[id].raw_format = format;
     defs[id].priority = priority;
     defs[id].flags = flags;
-    strcpy(defs[id].byteorder, byteorder);
+    snprintf(defs[i].byteorder, sizeof(defs[i].byteorder), "%s", byteorder);
   }
 
   return true;
@@ -364,17 +364,18 @@ static const char * const commandstrings[]={
 };
 
 
-static const char * preg(const ata_register & r, char * buf)
+static const char * preg(const ata_register & r, char (& buf)[8])
 {
   if (!r.is_set())
     //return "n/a ";
     return "....";
-  sprintf(buf, "0x%02x", r.val()); return buf;
+  snprintf(buf, sizeof(buf), "0x%02x", r.val());
+  return buf;
 }
 
 static void print_regs(const char * prefix, const ata_in_regs & r, const char * suffix = "\n")
 {
-  char bufs[7][4+1+13];
+  char bufs[7][8];
   pout("%s FR=%s, SC=%s, LL=%s, LM=%s, LH=%s, DEV=%s, CMD=%s%s", prefix,
     preg(r.features, bufs[0]), preg(r.sector_count, bufs[1]), preg(r.lba_low, bufs[2]),
     preg(r.lba_mid, bufs[3]), preg(r.lba_high, bufs[4]), preg(r.device, bufs[5]),
@@ -383,7 +384,7 @@ static void print_regs(const char * prefix, const ata_in_regs & r, const char * 
 
 static void print_regs(const char * prefix, const ata_out_regs & r, const char * suffix = "\n")
 {
-  char bufs[7][4+1+13];
+  char bufs[7][8];
   pout("%sERR=%s, SC=%s, LL=%s, LM=%s, LH=%s, DEV=%s, STS=%s%s", prefix,
     preg(r.error, bufs[0]), preg(r.sector_count, bufs[1]), preg(r.lba_low, bufs[2]),
     preg(r.lba_mid, bufs[3]), preg(r.lba_high, bufs[4]), preg(r.device, bufs[5]),
@@ -1636,11 +1637,11 @@ int ataSmartTest(ata_device * device, int testtype, bool force,
 
   //  Print ouf message that we are sending the command to test
   if (testtype==ABORT_SELF_TEST)
-    sprintf(cmdmsg,"Abort SMART off-line mode self-test routine");
+    snprintf(cmdmsg, sizeof(cmdmsg), "Abort SMART off-line mode self-test routine");
   else if (!type)
-    sprintf(cmdmsg, "SMART EXECUTE OFF-LINE IMMEDIATE subcommand 0x%02x", testtype);
+    snprintf(cmdmsg, sizeof(cmdmsg), "SMART EXECUTE OFF-LINE IMMEDIATE subcommand 0x%02x", testtype);
   else
-    sprintf(cmdmsg,"Execute SMART %s routine immediately in %s mode",type,captive);
+    snprintf(cmdmsg, sizeof(cmdmsg), "Execute SMART %s routine immediately in %s mode", type, captive);
   pout("Sending command: \"%s\".\n",cmdmsg);
 
   if (select) {
@@ -2660,8 +2661,9 @@ int ataPrintSmartSelfTestEntry(unsigned testnum, unsigned char test_type,
   char msglba[32];
   if (retval < 0 && failing_lba < 0xffffffffffffULL)
     snprintf(msglba, sizeof(msglba), "%"PRIu64, failing_lba);
-  else
-    strcpy(msglba, "-");
+  else {
+    msglba[0] = '-'; msglba[1] = 0;
+  }
 
   pout("#%2u  %-19s %-29s %1d0%%  %8u         %s\n", testnum,
        msgtest.c_str(), msgstat.c_str(), test_status & 0x0f, timestamp, msglba);

smartmontools/ataprint.cpp
@@ -60,10 +60,9 @@ static bool is_permissive()
 
 /* For the given Command Register (CR) and Features Register (FR), attempts
  * to construct a string that describes the contents of the Status
- * Register (ST) and Error Register (ER).  The caller passes the string
- * buffer and the return value is a pointer to this string.  If the
- * meanings of the flags of the error register are not known for the given
- * command then it returns NULL.
+ * Register (ST) and Error Register (ER).  If the meanings of the flags of
+ * the error register are not known for the given command then it returns an
+ * empty string.
  *
  * The meanings of the flags of the error register for all commands are
  * described in the ATA spec and could all be supported here in theory.
@@ -72,8 +71,7 @@ static bool is_permissive()
  * should probably be redesigned.
  */
 
-static const char * construct_st_er_desc(
-  char * s,
+static std::string format_st_er_desc(
   unsigned char CR, unsigned char FR,
   unsigned char ST, unsigned char ER,
   unsigned short SC,
@@ -110,6 +108,8 @@ static const char * construct_st_er_desc(
   /* A value of NULL means that the error flag isn't used */
   for (i = 0; i < 8; i++)
     error_flag[i] = NULL;
+
+  std::string str;
 
   switch (CR) {
   case 0x10:  // RECALIBRATE
@@ -281,7 +281,7 @@ static const char * construct_st_er_desc(
       error_flag[2] = abrt;
       break;
     default:
-      return NULL;
+      return str; // ""
       break;
     }
     break;
@@ -291,7 +291,7 @@ static const char * construct_st_er_desc(
       error_flag[2] = abrt;
       break;
     default:
-      return NULL;
+      return str; // ""
       break;
     }
     break;
@@ -320,39 +320,34 @@ static const char * construct_st_er_desc(
     error_flag[2] = abrt;
     break;
   default:
-    return NULL;
+    return str; // ""
   }
-
-  s[0] = '\0';
 
   /* We ignore any status flags other than Device Fault and Error */
 
   if (uses_device_fault && (ST & (1 << 5))) {
-    strcat(s, "Device Fault");
+    str = "Device Fault";
     if (ST & 1)  // Error flag
-      strcat(s, "; ");
+      str += "; ";
   }
   if (ST & 1) {  // Error flag
     int count = 0;
 
-    strcat(s, "Error: ");
+    str += "Error: ";
     for (i = 7; i >= 0; i--)
       if ((ER & (1 << i)) && (error_flag[i])) {
         if (count++ > 0)
-           strcat(s, ", ");
-        strcat(s, error_flag[i]);
+           str += ", ";
+        str += error_flag[i];
       }
   }
 
   // If the error was a READ or WRITE error, print the Logical Block
   // Address (LBA) at which the read or write failed.
   if (print_lba) {
-    char tmp[128];
     // print number of sectors, if known, and append to print string
-    if (print_sector) {
-      snprintf(tmp, 128, " %d sectors", print_sector);
-      strcat(s, tmp);
-    }
+    if (print_sector)
+      str += strprintf(" %d sectors", print_sector);
 
     if (lba28_regs) {
       unsigned lba;
@@ -367,8 +362,7 @@ static const char * construct_st_er_desc(
       lba <<= 8;
       // bits 0-7:   SN
       lba  |= lba28_regs->sector_number;
-      snprintf(tmp, 128, " at LBA = 0x%08x = %u", lba, lba);
-      strcat(s, tmp);
+      str += strprintf(" at LBA = 0x%08x = %u", lba, lba);
     }
     else if (lba48_regs) {
       // This assumes that upper LBA registers are 0 for 28-bit commands
@@ -386,18 +380,17 @@ static const char * construct_st_er_desc(
       lba48  |= lba48_regs->lba_mid_register;
       lba48 <<= 8;
       lba48  |= lba48_regs->lba_low_register;
-      snprintf(tmp, 128, " at LBA = 0x%08"PRIx64" = %"PRIu64, lba48, lba48);
-      strcat(s, tmp);
+      str += strprintf(" at LBA = 0x%08"PRIx64" = %"PRIu64, lba48, lba48);
     }
   }
 
-  return s;
+  return str;
 }
 
-static inline const char * construct_st_er_desc(char * s,
+static inline std::string format_st_er_desc(
   const ata_smart_errorlog_struct * data)
 {
-  return construct_st_er_desc(s,
+  return format_st_er_desc(
     data->commands[4].commandreg,
     data->commands[4].featuresreg,
     data->error_struct.status,
@@ -406,10 +399,10 @@ static inline const char * construct_st_er_desc(char * s,
     &data->error_struct, (const ata_smart_exterrlog_error *)0);
 }
 
-static inline const char * construct_st_er_desc(char * s,
+static inline std::string format_st_er_desc(
   const ata_smart_exterrlog_error_log * data)
 {
-  return construct_st_er_desc(s,
+  return format_st_er_desc(
     data->commands[4].command_register,
     data->commands[4].features_register,
     data->error.status_register,
@@ -1439,7 +1432,7 @@ static void print_device_statistics_page(const unsigned char * data, int page,
     }
     else {
       // Value not known (yet)
-      strcpy(valstr, "-");
+      valstr[0] = '-'; valstr[1] = 0;
     }
 
     pout("%3d  0x%03x  %d%c %15s%c %s\n",
@@ -1597,6 +1590,25 @@ static void PrintSataPhyEventCounters(const unsigned char * data, bool reset)
   pout("\n");
 }
 
+// Format milliseconds from error log entry as "DAYS+H:M:S.MSEC"
+static std::string format_milliseconds(unsigned msec)
+{
+  unsigned days  = msec  / 86400000U;
+  msec          -= days  * 86400000U;
+  unsigned hours = msec  / 3600000U;
+  msec          -= hours * 3600000U;
+  unsigned min   = msec  / 60000U;
+  msec          -= min   * 60000U;
+  unsigned sec   = msec  / 1000U;
+  msec          -= sec   * 1000U;
+
+  std::string str;
+  if (days)
+    str = strprintf("%2ud+", days);
+  str += strprintf("%02u:%02u:%02u.%03u", hours, min, sec, msec);
+  return str;
+}
+
 // Get description for 'state' value from SMART Error Logs
 static const char * get_error_log_state_desc(unsigned state)
 {
@@ -1693,10 +1705,9 @@ static int PrintSmartErrorlog(const ata_smart_errorlog *data,
            (int)summary->drive_head);
       // Add a description of the contents of the status and error registers
       // if possible
-      char descbuf[256];
-      const char * st_er_desc = construct_st_er_desc(descbuf, elog);
-      if (st_er_desc)
-        pout("  %s", st_er_desc);
+      std::string st_er_desc = format_st_er_desc(elog);
+      if (!st_er_desc.empty())
+        pout("  %s", st_er_desc.c_str());
       pout("\n\n");
       pout("  Commands leading to the command that caused the error were:\n"
            "  CR FR SC SN CL CH DH DC   Powered_Up_Time  Command/Feature_Name\n"
@@ -1706,11 +1717,6 @@ static int PrintSmartErrorlog(const ata_smart_errorlog *data,
 
         // Spec says: unused data command structures shall be zero filled
         if (nonempty(thiscommand, sizeof(*thiscommand))) {
-	  char timestring[32];
-	  
-	  // Convert integer milliseconds to a text-format string
-	  MsecToText(thiscommand->timestamp, timestring);
-	  
           pout("  %02x %02x %02x %02x %02x %02x %02x %02x  %16s  %s\n",
                (int)thiscommand->commandreg,
                (int)thiscommand->featuresreg,
@@ -1720,7 +1726,7 @@ static int PrintSmartErrorlog(const ata_smart_errorlog *data,
                (int)thiscommand->cylinder_high,
                (int)thiscommand->drive_head,
                (int)thiscommand->devicecontrolreg,
-	       timestring,
+               format_milliseconds(thiscommand->timestamp).c_str(),
                look_up_ata_command(thiscommand->commandreg, thiscommand->featuresreg));
 	}
       }
@@ -1838,10 +1844,9 @@ static int PrintSmartExtErrorLog(const ata_smart_exterrlog * log,
 
     // Add a description of the contents of the status and error registers
     // if possible
-    char descbuf[256];
-    const char * st_er_desc = construct_st_er_desc(descbuf, &entry);
-    if (st_er_desc)
-      pout("  %s", st_er_desc);
+    std::string st_er_desc = format_st_er_desc(&entry);
+    if (!st_er_desc.empty())
+      pout("  %s", st_er_desc.c_str());
     pout("\n\n");
 
     // Print command history
@@ -1856,9 +1861,6 @@ static int PrintSmartExtErrorLog(const ata_smart_exterrlog * log,
         continue;
 
       // Print registers, timestamp and ATA command name
-      char timestring[32];
-      MsecToText(cmd.timestamp, timestring);
-
       pout("  %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %16s  %s\n",
            cmd.command_register,
            cmd.features_register_hi,
@@ -1873,7 +1875,7 @@ static int PrintSmartExtErrorLog(const ata_smart_exterrlog * log,
            cmd.lba_low_register,
            cmd.device_register,
            cmd.device_control_register,
-           timestring,
+           format_milliseconds(cmd.timestamp).c_str(),
            look_up_ata_command(cmd.command_register, cmd.features_register));
     }
     pout("\n");
@@ -2081,16 +2083,15 @@ static void ataPrintSelectiveSelfTestLog(const ata_selective_self_test_log * log
 }
 
 // Format SCT Temperature value
-static const char * sct_ptemp(signed char x, char * buf)
+static const char * sct_ptemp(signed char x, char (& buf)[20])
 {
   if (x == -128 /*0x80 = unknown*/)
-    strcpy(buf, " ?");
-  else
-    sprintf(buf, "%2d", x);
+    return " ?";
+  snprintf(buf, sizeof(buf), "%2d", x);
   return buf;
 }
 
-static const char * sct_pbar(int x, char * buf)
+static const char * sct_pbar(int x, char (& buf)[64])
 {
   if (x <= 19)
     x = 0;
@@ -2169,7 +2170,7 @@ static int ataPrintSCTStatus(const ata_sct_status_response * sts)
 // Print SCT Temperature History Table
 static int ataPrintSCTTempHist(const ata_sct_temperature_history_table * tmh)
 {
-  char buf1[20], buf2[80];
+  char buf1[20], buf2[20], buf3[64];
   pout("SCT Temperature History Version:     %u%s\n", tmh->format_version,
        (tmh->format_version != 2 ? " (Unknown, should be 2)" : ""));
   pout("Temperature Sampling Period:         %u minute%s\n",
@@ -2209,11 +2210,11 @@ static int ataPrintSCTTempHist(const ata_sct_temperature_history_table * tmh)
         // TODO: Don't print times < boot time
         strftime(date, sizeof(date), "%Y-%m-%d %H:%M", localtime(&t));
         pout(" %3u    %s    %s  %s\n", i, date,
-          sct_ptemp(tmh->cb[i], buf1), sct_pbar(tmh->cb[i], buf2));
+          sct_ptemp(tmh->cb[i], buf1), sct_pbar(tmh->cb[i], buf3));
       }
       else if (n == n1+1) {
         pout(" ...    ..(%3u skipped).    ..  %s\n",
-          n2-n1-2, sct_pbar(tmh->cb[i], buf2));
+          n2-n1-2, sct_pbar(tmh->cb[i], buf3));
       }
       t += interval * 60; i = (i+1) % tmh->cb_size; n++;
     }

smartmontools/knowndrives.cpp
@@ -123,14 +123,16 @@ void drive_database::push_back(const drive_settings & src)
 
 const char * drive_database::copy_string(const char * src)
 {
-  char * dest = new char[strlen(src)+1];
+  size_t len = strlen(src);
+  char * dest = new char[len+1];
+  memcpy(dest, src, len+1);
   try {
     m_custom_strings.push_back(dest);
   }
   catch (...) {
     delete [] dest; throw;
   }
-  return strcpy(dest, src);
+  return dest;
 }
 
 

smartmontools/os_freebsd.cpp
@@ -11,8 +11,8 @@
  * any later version.
  *
  * You should have received a copy of the GNU General Public License
- * (for example COPYING); if not, write to the Free
- * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * (for example COPYING); If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <stdio.h>
@@ -334,7 +334,8 @@ bool freebsd_ata_device::ata_pass_through(const ata_cmd_in & in, ata_cmd_out & o
     {
       // We haven't gotten output that makes sense; print out some debugging info
       char buf[512];
-      sprintf(buf,"CMD=0x%02x\nFR =0x%02x\nNS =0x%02x\nSC =0x%02x\nCL =0x%02x\nCH =0x%02x\nRETURN =0x%04x\n",
+      snprintf(buf, sizeof(buf),
+        "CMD=0x%02x\nFR =0x%02x\nNS =0x%02x\nSC =0x%02x\nCL =0x%02x\nCH =0x%02x\nRETURN =0x%04x\n",
         (int)request.u.ata.command,
         (int)request.u.ata.feature,
         (int)request.u.ata.count,
@@ -856,7 +857,8 @@ int freebsd_highpoint_device::ata_command_interface(smart_command_set command, i
 
     // We haven't gotten output that makes sense; print out some debugging info
     char buf[512];
-    sprintf(buf,"CMD=0x%02x\nFR =0x%02x\nNS =0x%02x\nSC =0x%02x\nCL =0x%02x\nCH =0x%02x\nRETURN =0x%04x\n",
+    snprintf(buf, sizeof(buf),
+            "CMD=0x%02x\nFR =0x%02x\nNS =0x%02x\nSC =0x%02x\nCL =0x%02x\nCH =0x%02x\nRETURN =0x%04x\n",
             (int)pide_pt_hdr_out->command,
             (int)pide_pt_hdr_out->feature,
             (int)pide_pt_hdr_out->sectorcount,

smartmontools/os_linux.cpp
@@ -1302,7 +1302,7 @@ static int setup_3ware_nodes(const char *nodename, const char *driver_name)
 #endif
   /* Now check if nodes are correct */
   for (index=0; index<16; index++) {
-    sprintf(nodestring, "/dev/%s%d", nodename, index);
+    snprintf(nodestring, sizeof(nodestring), "/dev/%s%d", nodename, index);
 #ifdef WITH_SELINUX
     /* Get context of the node and set it as the default */
     if (selinux_enabled) {
@@ -1678,8 +1678,7 @@ public:
 };
 
 // Looks in /proc/scsi to suggest correct areca devices
-// If hint not NULL, return device path guess
-static int find_areca_in_proc(char *hint)
+static int find_areca_in_proc()
 {
     const char* proc_format_string="host\tchan\tid\tlun\ttype\topens\tqdepth\tbusy\tonline\n";
 
@@ -1718,9 +1717,6 @@ static int find_areca_in_proc(char *hint)
         dev++;
 	if (id == 16 && type == 3) {
 	   // devices with id=16 and type=3 might be Areca controllers
-	   if (!found && hint) {
-	       sprintf(hint, "/dev/sg%d", dev);
-	   }
 	   pout("Device /dev/sg%d appears to be an Areca controller.\n", dev);
            found++;
         }
@@ -1773,7 +1769,7 @@ int linux_areca_ata_device::arcmsr_do_scsi_io(struct scsi_cmnd_io * iop)
 
   if(!is_open()) {
       if(!open()){
-          find_areca_in_proc(NULL);
+          find_areca_in_proc();
       }
   }
 
@@ -1820,7 +1816,7 @@ int linux_areca_scsi_device::arcmsr_do_scsi_io(struct scsi_cmnd_io * iop)
 
   if(!is_open()) {
       if(!open()){
-          find_areca_in_proc(NULL);
+          find_areca_in_proc();
       }
   }
 

smartmontools/os_win32.cpp
@@ -785,7 +785,7 @@ smart_device * win_smart_interface::get_custom_smart_device(const char * name, c
       */
      for (int idx = 0; idx < ARECA_MAX_CTLR_NUM; idx++) {
         memset(devpath, 0, sizeof(devpath));
-        sprintf(devpath, "\\\\.\\scsi%d:", idx);
+        snprintf(devpath, sizeof(devpath), "\\\\.\\scsi%d:", idx);
         win_areca_ata_device *arcdev = new win_areca_ata_device(this, devpath, disknum, encnum);
         if(arcdev->arcmsr_probe()) {
           if(ctlrindex-- == 0) {
@@ -885,7 +885,7 @@ bool win_smart_interface::scan_smart_devices(smart_device_list & devlist,
 
   char name[20];
   for (int i = 0; i <= 9; i++) {
-    sprintf(name, "/dev/sd%c", 'a'+i);
+    snprintf(name, sizeof(name), "/dev/sd%c", 'a'+i);
     GETVERSIONINPARAMS_EX vers_ex;
 
     switch (get_phy_drive_type(i, (ata ? &vers_ex : 0))) {
@@ -904,7 +904,7 @@ bool win_smart_interface::scan_smart_devices(smart_device_list & devlist,
           int len = strlen(name);
           for (int pi = 0; pi < 32; pi++) {
             if (vers_ex.dwDeviceMapEx & (1L << pi)) {
-              sprintf(name+len, ",%u", pi);
+              snprintf(name+len, sizeof(name)-1-len, ",%u", pi);
               devlist.push_back( new win_ata_device(this, name, "ata") );
             }
           }
@@ -1537,7 +1537,7 @@ static int ata_via_3ware_miniport_ioctl(HANDLE hdevice, IDEREGS * regs, char * d
     return -1;
   }
   memset(&sb, 0, sizeof(sb));
-  strcpy((char *)sb.srbc.Signature, "<3ware>");
+  strncpy((char *)sb.srbc.Signature, "<3ware>", sizeof(sb.srbc.Signature));
   sb.srbc.HeaderLength = sizeof(SRB_IO_CONTROL);
   sb.srbc.Timeout = 60; // seconds
   sb.srbc.ControlCode = 0xA0000000;
@@ -1590,7 +1590,7 @@ static int update_3ware_devicemap_ioctl(HANDLE hdevice)
 {
   SRB_IO_CONTROL srbc;
   memset(&srbc, 0, sizeof(srbc));
-  strcpy((char *)srbc.Signature, "<3ware>");
+  strncpy((char *)srbc.Signature, "<3ware>", sizeof(srbc.Signature));
   srbc.HeaderLength = sizeof(SRB_IO_CONTROL);
   srbc.Timeout = 60; // seconds
   srbc.ControlCode = 0xCC010014;
@@ -3577,8 +3577,7 @@ bool win_areca_scsi_device::arcmsr_lock()
   if (sscanf(get_dev_name(), "\\\\.\\scsi%d:", &ctlrnum) < 1)
     return set_err(EINVAL, "unable to parse device name");
 
-  memset(mutexstr, 0, sizeof(mutexstr));
-  sprintf(mutexstr, "%s%d",SYNCOBJNAME, ctlrnum);
+  snprintf(mutexstr, sizeof(mutexstr), "%s%d", SYNCOBJNAME, ctlrnum);
   m_mutex = CreateMutex(NULL, FALSE, mutexstr);
   if ( m_mutex == NULL )
   {
@@ -3692,8 +3691,7 @@ bool win_areca_ata_device::arcmsr_lock()
   if (sscanf(get_dev_name(), "\\\\.\\scsi%d:", &ctlrnum) < 1)
     return set_err(EINVAL, "unable to parse device name");
 
-  memset(mutexstr, 0, sizeof(mutexstr));
-  sprintf(mutexstr, "%s%d",SYNCOBJNAME, ctlrnum);
+  snprintf(mutexstr, sizeof(mutexstr), "%s%d", SYNCOBJNAME, ctlrnum);
   m_mutex = CreateMutex(NULL, FALSE, mutexstr);
   if ( m_mutex == NULL )
   {

smartmontools/scsicmds.cpp
@@ -15,8 +15,7 @@
  * any later version.
  *
  * You should have received a copy of the GNU General Public License
- * (for example COPYING); if not, write to the Free
- * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * (for example COPYING); If not, see <http://www.gnu.org/licenses/>.
  *
  * This code was originally developed as a Senior Thesis by Michael Cornwell
  * at the Concurrent Systems Laboratory (now part of the Storage Systems
@@ -71,7 +70,7 @@ void dStrHex(const char* str, int len, int no_ascii)
     if (len <= 0) return;
     memset(buff,' ',80);
     buff[80]='\0';
-    k = sprintf(buff + 1, "%.2x", a);
+    k = snprintf(buff+1, sizeof(buff)-1, "%.2x", a);
     buff[k + 1] = ' ';
     if (bpos >= ((bpstart + (9 * 3))))
         bpos++;
@@ -82,7 +81,7 @@ void dStrHex(const char* str, int len, int no_ascii)
         bpos += 3;
         if (bpos == (bpstart + (9 * 3)))
             bpos++;
-        sprintf(&buff[bpos], "%.2x", (int)(unsigned char)c);
+        snprintf(buff+bpos, sizeof(buff)-bpos, "%.2x", (int)(unsigned char)c);
         buff[bpos + 2] = ' ';
         if (no_ascii)
             buff[cpos++] = ' ';
@@ -98,7 +97,7 @@ void dStrHex(const char* str, int len, int no_ascii)
             cpos = cpstart;
             a += 16;
             memset(buff,' ',80);
-            k = sprintf(buff + 1, "%.2x", a);
+            k = snprintf(buff+1, sizeof(buff)-1, "%.2x", a);
             buff[k + 1] = ' ';
         }
     }
@@ -296,7 +295,7 @@ int scsi_decode_lu_dev_id(const unsigned char * b, int blen, char * s,
     int m, c_set, assoc, desig_type, i_len, naa, off, u, have_scsi_ns;
     const unsigned char * ucp;
     const unsigned char * ip;
-    char * orig_s = s;
+    int si = 0;
 
     if (transport)
 	*transport = -1;
@@ -312,7 +311,7 @@ int scsi_decode_lu_dev_id(const unsigned char * b, int blen, char * s,
         ucp = b + off;
         i_len = ucp[3];
         if ((off + i_len + 4) > blen) {
-	    s += sprintf(s, "error: designator length");
+            snprintf(s+si, slen-si, "error: designator length");
 	    return -1;
         }
         assoc = ((ucp[1] >> 4) & 0x3);
@@ -330,52 +329,52 @@ int scsi_decode_lu_dev_id(const unsigned char * b, int blen, char * s,
             break;
         case 2: /* EUI-64 based */
             if ((8 != i_len) && (12 != i_len) && (16 != i_len)) {
-	        s += sprintf(s, "error: EUI-64 length");
+                snprintf(s+si, slen-si, "error: EUI-64 length");
 	        return -1;
 	    }
 	    if (have_scsi_ns)
-		s = orig_s;
-            s += sprintf(s, "0x");
+                si = 0;
+            si += snprintf(s+si, slen-si, "0x");
             for (m = 0; m < i_len; ++m)
-                s += sprintf(s, "%02x", (unsigned int)ip[m]);
+                si += snprintf(s+si, slen-si, "%02x", (unsigned int)ip[m]);
             break;
         case 3: /* NAA */
             if (1 != c_set) {
-	        s += sprintf(s, "error: NAA bad code_set");
+                snprintf(s+si, slen-si, "error: NAA bad code_set");
 		return -1;
 	    }
             naa = (ip[0] >> 4) & 0xff;
             if ((naa < 2) || (naa > 6) || (4 == naa)) {
-	        s += sprintf(s, "error: unexpected NAA");
+                snprintf(s+si, slen-si, "error: unexpected NAA");
 		return -1;
             }
 	    if (have_scsi_ns)
-		s = orig_s;
+                si = 0;
             if (2 == naa) {             /* NAA IEEE Extended */
                 if (8 != i_len) {
-	            s += sprintf(s, "error: NAA 2 length");
+                    snprintf(s+si, slen-si, "error: NAA 2 length");
 		    return -1;
                 }
-                s += sprintf(s, "0x");
+                si += snprintf(s+si, slen-si, "0x");
                 for (m = 0; m < 8; ++m)
-                    s += sprintf(s, "%02x", (unsigned int)ip[m]);
+                    si += snprintf(s+si, slen-si, "%02x", (unsigned int)ip[m]);
             } else if ((3 == naa ) || (5 == naa)) {
                 /* NAA=3 Locally assigned; NAA=5 IEEE Registered */
                 if (8 != i_len) {
-	            s += sprintf(s, "error: NAA 3 or 5 length");
+                    snprintf(s+si, slen-si, "error: NAA 3 or 5 length");
 		    return -1;
                 }
-                s += sprintf(s, "0x");
+                si += snprintf(s+si, slen-si, "0x");
                 for (m = 0; m < 8; ++m)
-                    s += sprintf(s, "%02x", (unsigned int)ip[m]);
+                    si += snprintf(s+si, slen-si, "%02x", (unsigned int)ip[m]);
             } else if (6 == naa) {      /* NAA IEEE Registered extended */
                 if (16 != i_len) {
-	            s += sprintf(s, "error: NAA 6 length");
+                    snprintf(s+si, slen-si, "error: NAA 6 length");
 		    return -1;
                 }
-                s += sprintf(s, "0x");
+                si += snprintf(s+si, slen-si, "0x");
                 for (m = 0; m < 16; ++m)
-                    s += sprintf(s, "%02x", (unsigned int)ip[m]);
+                    si += snprintf(s+si, slen-si, "%02x", (unsigned int)ip[m]);
             }
             break;
         case 4: /* Relative target port */
@@ -385,12 +384,12 @@ int scsi_decode_lu_dev_id(const unsigned char * b, int blen, char * s,
             break;
         case 8: /* SCSI name string */
             if (3 != c_set) {
-	        s += sprintf(s, "error: SCSI name string");
+                snprintf(s+si, slen-si, "error: SCSI name string");
 		return -1;
             }
             /* does %s print out UTF-8 ok?? */
-	    if (orig_s == s) {
-                s += sprintf(s, "%s", (const char *)ip);
+            if (si == 0) {
+                si += snprintf(s+si, slen-si, "%s", (const char *)ip);
 		++have_scsi_ns;
 	    }
             break;
@@ -399,7 +398,7 @@ int scsi_decode_lu_dev_id(const unsigned char * b, int blen, char * s,
         }
     }
     if (-2 == u) {
-        s += sprintf(s, "error: bad structure");
+        snprintf(s+si, slen-si, "error: bad structure");
 	return -1;
     }
     return 0;

commit 0798caf27692a7c6ed4556eeaaf09f8c905deb35

atacmds.cpp: Fix assignment of BYTEORDER from -v option (Regression from r3719).
svn-path: trunk@3747

smartmontools/atacmds.cpp
@@ -35,7 +35,7 @@
 #include "utility.h"
 #include "dev_ata_cmd_set.h" // for parsed_ata_device
 
-const char * atacmds_cpp_cvsid = "$Id: atacmds.cpp 3740 2013-01-01 18:12:00Z chrfranke $"
+const char * atacmds_cpp_cvsid = "$Id: atacmds.cpp 3747 2013-01-16 21:29:42Z chrfranke $"
                                  ATACMDS_H_CVSID;
 
 // Print ATA debug messages?
@@ -239,7 +239,7 @@ bool parse_attribute_def(const char * opt, ata_vendor_attr_defs & defs,
     defs[id].raw_format = format;
     defs[id].priority = priority;
     defs[id].flags = flags;
-    snprintf(defs[i].byteorder, sizeof(defs[i].byteorder), "%s", byteorder);
+    snprintf(defs[id].byteorder, sizeof(defs[id].byteorder), "%s", byteorder);
   }
 
   return true;