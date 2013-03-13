296e62f15076a2adf97eb1d7fc01edb5e5682195
chrfranke <authors@smartmontools.org>
2013-03-13 20:00:51 +0000

    drivedb.h: - SandForce Driven SSDs: Fix format of attribute 198 (ticket #258).
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3789 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/drivedb.h
+++ smartmontools/drivedb.h
@@ -311,7 +311,7 @@ const drive_settings builtin_knowndrives[] = {
   //"-v 194,tempminmax,Temperature_Celsius "
     "-v 195,raw24/raw32,ECC_Uncorr_Error_Count "
   //"-v 196,raw16(raw16),Reallocated_Event_Count "
-    "-v 198,hex48,Uncorrectable_Sector_Ct "
+    "-v 198,raw24/raw32:210zr54,Uncorrectable_Sector_Ct " // KINGSTON SE100S3100G/510ABBF0
     "-v 199,raw48,SATA_CRC_Error_Count "
     "-v 201,raw24/raw32,Unc_Soft_Read_Err_Rate "
     "-v 204,raw24/raw32,Soft_ECC_Correct_Rate "
