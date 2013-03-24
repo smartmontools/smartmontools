28ddb0cdbfa4a67bd877e1a5d43ec64f35ca46b3
chrfranke <authors@smartmontools.org>
2013-03-24 18:52:54 +0000

    dev_areca.cpp: Add casts to silence C++11 -Wnarrowing warning from g++ 4.8.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3803 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/dev_areca.cpp
+++ smartmontools/dev_areca.cpp
@@ -356,7 +356,8 @@ int generic_areca_device::arcmsr_get_dev_type()
   int ctlr_type = -1;
   int encnum = get_encnum();
   int disknum = get_disknum();
-  unsigned char areca_packet[] = {0x5E, 0x01, 0x61, 0x03, 0x00, 0x22, disknum - 1, encnum - 1, 0x00};
+  unsigned char areca_packet[] = {0x5E, 0x01, 0x61, 0x03, 0x00, 0x22,
+    (unsigned char)(disknum - 1), (unsigned char)(encnum - 1), 0x00};
 
   memset(return_buff, 0, sizeof(return_buff));
   expected = arcmsr_ui_handler(areca_packet, sizeof(areca_packet), return_buff);
