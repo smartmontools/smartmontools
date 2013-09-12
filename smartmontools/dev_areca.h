07e17208a465962046b77190f8def0a3f7c27844
chrfranke <authors@smartmontools.org>
2013-09-12 05:36:20 +0000

    dev_areca.h: Fix build on kFreeBSD (Debian bug 717567). Patch was provided by Christoph Egger.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@3854 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/dev_areca.h
+++ smartmontools/dev_areca.h
@@ -58,7 +58,7 @@ ARCMSR_CMD_TOTAL
 #define ARCMSR_IOCTL_CLEAR_RQBUFFER          (ARECA_SATA_RAID | FUNCTION_CLEAR_RQBUFFER)
 #define ARCMSR_IOCTL_CLEAR_WQBUFFER          (ARECA_SATA_RAID | FUNCTION_CLEAR_WQBUFFER)
 #define ARCMSR_IOCTL_RETURN_CODE_3F          (ARECA_SATA_RAID | FUNCTION_RETURN_CODE_3F)
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 #include <sys/ioctl.h> // _IOWR
 
 /*FunctionCode*/
