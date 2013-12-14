617ae7af3474bfc93d4a46beea25e39c353f2306
chrfranke <authors@smartmontools.org>
2013-12-14 21:30:40 +0000

    Merge r3865 into 5.40-6.0 drivedb.h branches.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/branches/RELEASE_5_40_DRIVEDB@3866 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/drivedb.h
+++ smartmontools/drivedb.h
@@ -1920,8 +1920,8 @@ const drive_settings builtin_knowndrives[] = {
     "", "", ""
   },
   { "Toshiba 2.5\" HDD MK..65GSX", // tested with TOSHIBA MK5065GSX/GJ003A, MK3265GSXN/GH012H,
-      // MK5065GSXF/GP006B
-    "TOSHIBA MK(16|25|32|50|64)65GSX[FN]?",
+      // MK5065GSXF/GP006B, MK2565GSX H/GJ003A
+    "TOSHIBA MK(16|25|32|50|64)65GSX[FN]?( H)?", // "... H" = USB ?
     "", "", ""
   },
   { "Toshiba 2.5\" HDD MK..76GSX", // tested with TOSHIBA MK3276GSX/GS002D
@@ -1932,6 +1932,10 @@ const drive_settings builtin_knowndrives[] = {
   },
   { "Toshiba 2.5\" HDD MQ01ABD...", // tested with TOSHIBA MQ01ABD100/AX001U
     "TOSHIBA MQ01ABD(025|032|050|064|075|100)",
+    "", "", ""
+  },
+  { "Toshiba 2.5\" HDD MQ01UBD... (USB 3.0)", // tested with TOSHIBA MQ01ABD100/AX001U
+    "TOSHIBA MQ01UBD(050|075|100)",
     "", "", ""
   },
   { "Toshiba 3.5\" HDD MK.002TSKB", // tested with TOSHIBA MK1002TSKB/MT1A
@@ -2722,6 +2726,12 @@ const drive_settings builtin_knowndrives[] = {
     "-d sat"
   },
   // Toshiba
+  { "USB: Toshiba Stor.E Slim USB 3.0; ", // 1TB, MQ01UBD100
+    "0x0480:0x0100",
+    "", // 0x0000
+    "",
+    "-d sat"
+  },
   { "USB: Toshiba Canvio 500GB; SunPlus",
     "0x0480:0xa004",
     "",
@@ -2737,6 +2747,12 @@ const drive_settings builtin_knowndrives[] = {
   { "USB: Toshiba Canvio 3.0 Portable Hard Drive; ", // 1TB
     "0x0480:0xa007",
     "", // 0x0001
+    "",
+    "-d sat"
+  },
+  { "USB: Toshiba Stor.E Basics; ", // 1TB
+    "0x0480:0xa009",
+    "",
     "",
     "-d sat"
   },
@@ -3076,7 +3092,7 @@ const drive_settings builtin_knowndrives[] = {
   },
   // Lumberg, Inc.
   { "USB: Toshiba Stor.E; Sunplus",
-    "0x0939:0x0b16",
+    "0x0939:0x0b1[56]",
     "",
     "",
     "-d usbsunplus"
@@ -3131,7 +3147,7 @@ const drive_settings builtin_knowndrives[] = {
     "-d sat"
   },
   { "USB: Seagate FreeAgent GoFlex USB 2.0; ",
-    "0x0bc2:0x5021",
+    "0x0bc2:0x502[01]",
     "",
     "",
     "-d sat"
@@ -3298,6 +3314,12 @@ const drive_settings builtin_knowndrives[] = {
     "",
     "-d sat"
   },
+  { "USB: WD My Passport Ultra; ",
+    "0x1058:0x0741",
+    "",
+    "",
+    "-d sat"
+  },
   { "USB: WD My Passport USB 3.0; ",
     "0x1058:0x07[4a]8",
     "",
@@ -3329,7 +3351,7 @@ const drive_settings builtin_knowndrives[] = {
     "-d sat"
   },
   { "USB: WD Elements; ",
-    "0x1058:0x10(10|a2)",
+    "0x1058:0x10(10|48|a2)",
     "", // 0x0105
     "",
     "-d sat"
@@ -3445,6 +3467,12 @@ const drive_settings builtin_knowndrives[] = {
     "",
     "-d sat"
   },
+  { "USB: ; Initio",
+    "0x13fd:0x1640",
+    "", // 0x0864
+    "",
+    "-d sat,12" // some SMART commands fail, see ticket #295
+  },
   { "USB: Intenso Memory Station 2,5\"; Initio",
     "0x13fd:0x1840",
     "",
@@ -3550,6 +3578,12 @@ const drive_settings builtin_knowndrives[] = {
   { "USB: ; LucidPORT USB300", // RaidSonic ICY BOX IB-110StU3-B, Sharkoon SATA QuickPort H3
     "0x1759:0x500[02]", // 0x5000: USB 2.0, 0x5002: USB 3.0
     "",
+    "",
+    "-d sat"
+  },
+  { "USB: ; LucidPort", // Fuj:tech SATA-USB3 dock
+    "0x1759:0x5100",
+    "", // 0x2580
     "",
     "-d sat"
   },
