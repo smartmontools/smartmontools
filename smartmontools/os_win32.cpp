204f03cb98de78a25e31430b0629e776aab57de4
Christian Franke <christian.franke@t-online.de>
2014-06-18 16:23:56 +0000

    os_win32.cpp: Fix CSMI support for older Intel RST drivers which set bPortIdentifier=0xff (regression from r3888).
    svn-path: trunk@3910

--- smartmontools/os_win32.cpp
+++ smartmontools/os_win32.cpp
@@ -95,7 +95,7 @@
 #define SELECT_WIN_32_64(x32, x64) (x64)
 #endif
 
-const char * os_win32_cpp_cvsid = "$Id: os_win32.cpp 3888 2014-04-10 19:36:53Z chrfranke $";
+const char * os_win32_cpp_cvsid = "$Id: os_win32.cpp 3910 2014-06-18 16:23:56Z chrfranke $";
 
 /////////////////////////////////////////////////////////////////////////////
 // Windows I/O-controls, some declarations are missing in the include files
@@ -2939,10 +2939,12 @@ unsigned csmi_device::get_ports_used()
       default:
         continue;
     }
-    if (pe.bPortIdentifier >= 32)
-      continue;
 
-    ports_used |= (1 << pe.bPortIdentifier);
+    if (pe.bPortIdentifier == 0xff)
+      // Older (<= 9.*) Intel RST driver
+      ports_used |= (1 << i);
+    else
+      ports_used |= (1 << pe.bPortIdentifier);
   }
 
   return ports_used;
@@ -2961,10 +2963,21 @@ bool csmi_device::select_port(int port)
     const CSMI_SAS_PHY_ENTITY & pe = phy_info.Phy[i];
     if (pe.Identify.bDeviceType == CSMI_SAS_NO_DEVICE_ATTACHED)
       continue;
-    if (pe.bPortIdentifier > max_port)
-      max_port = pe.bPortIdentifier;
-    if (pe.bPortIdentifier != port)
-      continue;
+
+    if (pe.bPortIdentifier == 0xff) {
+      // Older (<= 9.*) Intel RST driver
+      max_port = phy_info.bNumberOfPhys - 1;
+      if (i >= phy_info.bNumberOfPhys)
+        break;
+      if ((int)i != port)
+        continue;
+    }
+    else {
+      if (pe.bPortIdentifier > max_port)
+        max_port = pe.bPortIdentifier;
+      if (pe.bPortIdentifier != port)
+        continue;
+    }
 
     port_index = i;
     break;
