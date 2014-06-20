a82b519972c61476c0755de1c88ee65a09e53b3f
Christian Franke <christian.franke@t-online.de>
2014-06-20 19:21:00 +0000

    scsiata.cpp: usbjmicron_device: Check SCSI resid for SMART STATUS. Some (Prolific) USB bridges do not transfer a status byte. os_win32.cpp: Include SCSI resid in debug output.
    svn-path: trunk@3916

--- smartmontools/os_win32.cpp
+++ smartmontools/os_win32.cpp
@@ -95,7 +95,7 @@
 #define SELECT_WIN_32_64(x32, x64) (x64)
 #endif
 
-const char * os_win32_cpp_cvsid = "$Id: os_win32.cpp 3910 2014-06-18 16:23:56Z chrfranke $";
+const char * os_win32_cpp_cvsid = "$Id: os_win32.cpp 3916 2014-06-20 19:21:00Z chrfranke $";
 
 /////////////////////////////////////////////////////////////////////////////
 // Windows I/O-controls, some declarations are missing in the include files
@@ -3492,7 +3492,7 @@ bool win_scsi_device::scsi_pass_through(struct scsi_cmnd_io * iop)
 
   if ((iop->dxfer_dir == DXFER_FROM_DEVICE) && (report > 1)) {
      int trunc = (iop->dxfer_len > 256) ? 1 : 0;
-     pout("  Incoming data, len=%d%s:\n", (int)iop->dxfer_len,
+     pout("  Incoming data, len=%d, resid=%d%s:\n", (int)iop->dxfer_len, iop->resid,
         (trunc ? " [only first 256 bytes shown]" : ""));
         dStrHex(iop->dxferp, (trunc ? 256 : iop->dxfer_len) , 1);
   }
@@ -3618,7 +3618,7 @@ static long scsi_pass_through_direct(HANDLE fd, UCHAR targetid, struct scsi_cmnd
 
   if ((iop->dxfer_dir == DXFER_FROM_DEVICE) && (report > 1)) {
      int trunc = (iop->dxfer_len > 256) ? 1 : 0;
-     pout("  Incoming data, len=%d%s:\n", (int)iop->dxfer_len,
+     pout("  Incoming data, len=%d, resid=%d%s:\n", (int)iop->dxfer_len, iop->resid,
         (trunc ? " [only first 256 bytes shown]" : ""));
         dStrHex(iop->dxferp, (trunc ? 256 : iop->dxfer_len) , 1);
   }
