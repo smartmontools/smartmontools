dac65eb7c27948cd25b8384c33252909855bed1d
Christian Franke <christian.franke@t-online.de>
2015-10-14 19:05:19 +0000

    configure.ac: Disable os_solaris_ata.o by default. Add --with-solaris-sparc-ata option to enable. Makefile.am: Exclude os_solaris_ata.s from source tarball. os_solaris.cpp: Check for WITH_SOLARIS_SPARC_ATA instead of __sparc.
    svn-path: trunk@4142

--- smartmontools/os_solaris.cpp
+++ smartmontools/os_solaris.cpp
@@ -39,9 +39,9 @@
 
 extern long long bytes;
 
-static const char *filenameandversion="$Id: os_solaris.cpp 4140 2015-10-13 05:59:52Z chrfranke $";
+static const char *filenameandversion="$Id: os_solaris.cpp 4142 2015-10-14 19:05:19Z chrfranke $";
 
-const char *os_XXXX_c_cvsid="$Id: os_solaris.cpp 4140 2015-10-13 05:59:52Z chrfranke $" \
+const char *os_XXXX_c_cvsid="$Id: os_solaris.cpp 4142 2015-10-14 19:05:19Z chrfranke $" \
 ATACMDS_H_CVSID CONFIG_H_CVSID INT64_H_CVSID OS_SOLARIS_H_CVSID SCSICMDS_H_CVSID UTILITY_H_CVSID;
 
 // The printwarning() function warns about unimplemented functions
@@ -272,7 +272,7 @@ int deviceclose(int fd){
     return close(fd);
 }
 
-#if defined(__sparc)
+#if defined(WITH_SOLARIS_SPARC_ATA)
 // swap each 2-byte pairs in a sector
 static void swap_sector(void *p)
 {
@@ -287,7 +287,7 @@ static void swap_sector(void *p)
 
 // Interface to ATA devices.  See os_linux.c
 int ata_command_interface(int fd, smart_command_set command, int select, char *data){
-#if defined(__sparc)
+#if defined(WITH_SOLARIS_SPARC_ATA)
     int err;
  
     switch (command){
@@ -329,7 +329,7 @@ int ata_command_interface(int fd, smart_command_set command, int select, char *d
 	EXIT(1);
 	break;
     }
-#else /* __sparc */
+#else /* WITH_SOLARIS_SPARC_ATA */
     ARGUSED(fd); ARGUSED(command); ARGUSED(select); ARGUSED(data);
 
     /* Above smart_* routines uses undocumented ioctls of "dada"
