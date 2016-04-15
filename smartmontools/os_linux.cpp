b6094eb4f6b9902b0b1853c061f81f41cd11b494
Yuriy M. Kaminskiy <>
2016-04-15 05:38:04 +0000

    os_linux.cpp: Fix harmless bug in errno check of HDIO_DRIVE_TASK* ioctl() calls.  Bug was introduced 12 years ago in r1609, the fix in r4003 was incomplete.
    Patch was provided by Yuriy M. Kaminskiy.
    
    svn-path: trunk@4294

--- smartmontools/os_linux.cpp
+++ smartmontools/os_linux.cpp
@@ -102,7 +102,7 @@
 
 #define ARGUSED(x) ((void)(x))
 
-const char * os_linux_cpp_cvsid = "$Id: os_linux.cpp 4274 2016-04-02 15:41:26Z chrfranke $"
+const char * os_linux_cpp_cvsid = "$Id: os_linux.cpp 4294 2016-04-15 05:38:04Z chrfranke $"
   OS_LINUX_H_CVSID;
 extern unsigned char failuretest_permissive;
 
@@ -380,7 +380,7 @@ int linux_ata_device::ata_command_interface(smart_command_set command, int selec
     memcpy(task+sizeof(ide_task_request_t), data, 512);
 
     if (ioctl(get_fd(), HDIO_DRIVE_TASKFILE, task)) {
-      if (errno==-EINVAL)
+      if (errno==EINVAL)
         pout("Kernel lacks HDIO_DRIVE_TASKFILE support; compile kernel with CONFIG_IDE_TASKFILE_IO set\n");
       return -1;
     }
@@ -406,7 +406,7 @@ int linux_ata_device::ata_command_interface(smart_command_set command, int selec
     buff[5]=normal_hi;
 
     if (ioctl(get_fd(), HDIO_DRIVE_TASK, buff)) {
-      if (errno==-EINVAL) {
+      if (errno==EINVAL) {
         pout("Error SMART Status command via HDIO_DRIVE_TASK failed");
         pout("Rebuild older linux 2.2 kernels with HDIO_DRIVE_TASK support added\n");
       }
