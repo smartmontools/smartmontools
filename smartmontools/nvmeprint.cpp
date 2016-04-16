fa7e4fe72b323a180a42ccd2dc322c67eb0e30f4
chrfranke <authors@smartmontools.org>
2016-04-16 17:02:45 +0000

    nvmeprint.cpp: Add Power State and Namespace info to '-i' output. Do not print unset or duplicate info unless debug mode is enabled.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4298 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/nvmeprint.cpp
+++ smartmontools/nvmeprint.cpp
@@ -31,25 +31,25 @@ const char * nvmeprint_cvsid = "$Id$"
 
 using namespace smartmontools;
 
-// Format 128 bit LE integer for printing.
-// Add value with SI prefixes if BYTES_PER_UNIT is specified.
-static const char * le128_to_str(char (& str)[64], const unsigned char (& val)[16],
-  unsigned bytes_per_unit = 0)
+// Return true if 128 bit LE integer is != 0.
+static bool le128_is_non_zero(const unsigned char (& val)[16])
 {
-  uint64_t hi = val[15];
-  for (int i = 15-1; i >= 8; i--) {
-    hi <<= 8; hi += val[i];
+  for (int i = 0; i < 16; i++) {
+    if (val[i])
+      return true;
   }
-  uint64_t lo = val[7];
-  for (int i =  7-1; i >= 0; i--) {
-    lo <<= 8; lo += val[i];
-  }
+  return false;
+}
 
+// Format 128 bit integer for printing.
+// Add value with SI prefixes if BYTES_PER_UNIT is specified.
+static const char * le128_to_str(char (& str)[64], uint64_t hi, uint64_t lo, unsigned bytes_per_unit)
+{
   if (!hi) {
     // Up to 64-bit, print exact value
     format_with_thousands_sep(str, sizeof(str)-16, lo);
 
-    if (bytes_per_unit && lo < 0xffffffffffffffffULL / bytes_per_unit) {
+    if (lo && bytes_per_unit && lo < 0xffffffffffffffffULL / bytes_per_unit) {
       int i = strlen(str);
       str[i++] = ' '; str[i++] = '[';
       format_capacity(str+i, (int)sizeof(str)-i-1, lo * bytes_per_unit);
@@ -64,6 +64,28 @@ static const char * le128_to_str(char (& str)[64], const unsigned char (& val)[1
   }
 
   return str;
+}
+
+// Format 128 bit LE integer for printing.
+// Add value with SI prefixes if BYTES_PER_UNIT is specified.
+static const char * le128_to_str(char (& str)[64], const unsigned char (& val)[16],
+  unsigned bytes_per_unit = 0)
+{
+  uint64_t hi = val[15];
+  for (int i = 15-1; i >= 8; i--) {
+    hi <<= 8; hi += val[i];
+  }
+  uint64_t lo = val[7];
+  for (int i =  7-1; i >= 0; i--) {
+    lo <<= 8; lo += val[i];
+  }
+  return le128_to_str(str, hi, lo, bytes_per_unit);
+}
+
+// Format capacity specified as 64bit LBA count for printing.
+static const char * lbacap_to_str(char (& str)[64], uint64_t lba_cnt, int lba_bits)
+{
+  return le128_to_str(str, (lba_cnt >> (64 - lba_bits)), (lba_cnt << lba_bits), 1);
 }
 
 // Format a Kelvin temperature value in Celsius.
@@ -81,23 +103,94 @@ static inline unsigned le16_to_uint(const unsigned char (& val)[2])
   return ((val[1] << 8) | val[0]);
 }
 
-static void print_id_ctrl(const nvme_id_ctrl & id_ctrl)
+static void print_id_ctrl_ns(const nvme_id_ctrl & id_ctrl, const nvme_id_ns & id_ns,
+  unsigned nsid, bool show_all)
 {
   char buf[64];
   pout("Model Number:                       %s\n", format_char_array(buf, id_ctrl.mn));
   if (!dont_print_serial_number)
     pout("Serial Number:                      %s\n", format_char_array(buf, id_ctrl.sn));
   pout("Firmware Version:                   %s\n", format_char_array(buf, id_ctrl.fr));
-  pout("PCI Vendor ID:                      0x%04x:0x%04x\n", id_ctrl.vid, id_ctrl.ssvid);
+
+  // Vendor and Subsystem IDs are usually equal
+  if (show_all || id_ctrl.vid != id_ctrl.ssvid) {
+    pout("PCI Vendor ID:                      0x%04x\n", id_ctrl.vid);
+    pout("PCI Vendor Subsystem ID:            0x%04x\n", id_ctrl.ssvid);
+  }
+  else {
+    pout("PCI Vendor/Subsystem ID:            0x%04x\n", id_ctrl.vid);
+  }
+
   pout("IEEE OUI Identifier:                0x%02x%02x%02x\n",
-       id_ctrl.ieee[0], id_ctrl.ieee[1], id_ctrl.ieee[2]);
-  pout("Total NVM Capacity:                 %s\n", le128_to_str(buf, id_ctrl.tnvmcap, 1));
-  pout("Unallocated NVM Capacity:           %s\n", le128_to_str(buf, id_ctrl.unvmcap, 1));
-  pout("Maximum Data Transfer Size:         %d\n", id_ctrl.mdts);
-  pout("Number of Namespaces:               %u\n", id_ctrl.nn);
+       id_ctrl.ieee[2], id_ctrl.ieee[1], id_ctrl.ieee[0]);
+
+  // Capacity info is optional for devices without namespace management
+  if (show_all || le128_is_non_zero(id_ctrl.tnvmcap) || le128_is_non_zero(id_ctrl.unvmcap)) {
+    pout("Total NVM Capacity:                 %s\n", le128_to_str(buf, id_ctrl.tnvmcap, 1));
+    pout("Unallocated NVM Capacity:           %s\n", le128_to_str(buf, id_ctrl.unvmcap, 1));
+  }
+
+  if (show_all || id_ctrl.mdts) {
+    if (id_ctrl.mdts)
+      pout("Maximum Data Transfer Size:         %u Pages\n", (1U << id_ctrl.mdts));
+    else
+      pout("Maximum Data Transfer Size:         [no limit]\n");
+  }
+
   pout("Controller ID:                      %d\n", id_ctrl.cntlid);
-  pout("Warning  Comp. Temp. Threshold:     %s\n", kelvin_to_str(buf, id_ctrl.wctemp));
-  pout("Critical Comp. Temp. Threshold:     %s\n", kelvin_to_str(buf, id_ctrl.cctemp));
+
+  // Temperature thresholds are optional
+  if (show_all || id_ctrl.wctemp)
+    pout("Warning  Comp. Temp. Threshold:     %s\n", kelvin_to_str(buf, id_ctrl.wctemp));
+  if (show_all || id_ctrl.cctemp)
+    pout("Critical Comp. Temp. Threshold:     %s\n", kelvin_to_str(buf, id_ctrl.cctemp));
+
+  // Print Power States
+  if (show_all || id_ctrl.npss || id_ctrl.psd[0].max_power) {
+    for (int i = 0; i <= id_ctrl.npss /* 1-based */ && i < 32; i++) {
+      if (id_ctrl.psd[i].flags & 0x01)
+        snprintf(buf, sizeof(buf), "%d.%04d", id_ctrl.psd[i].max_power / 10000,
+                                              id_ctrl.psd[i].max_power % 10000);
+      else
+        snprintf(buf, sizeof(buf), "%d.%02d", id_ctrl.psd[i].max_power / 100,
+                                              id_ctrl.psd[i].max_power % 100);
+      pout("Power State %d Maximum Power:       %s%s W%s\n", i, (i < 10 ? " " : ""),
+           buf, (id_ctrl.npss && !(id_ctrl.psd[i].flags & 0x02) ? " (active)" : ""));
+    }
+  }
+
+  // Print namespace info if available
+  pout("Number of Namespaces:               %u\n", id_ctrl.nn);
+
+  if (nsid && id_ns.nsze) {
+    const char * align = "  " + (nsid < 10 ? 0 : (nsid < 100 ? 1 : 2));
+    int fmt_lba_bits = id_ns.lbaf[id_ns.flbas & 0xf].ds;
+
+    pout("Namespace %u Size:                 %s%s\n", nsid, align,
+         lbacap_to_str(buf, id_ns.nsze, fmt_lba_bits));
+    // Capacity and Utilization may be always equal if thin provisioning is not supported
+    if (show_all || id_ns.ncap != id_ns.nsze)
+      pout("Namespace %u Capacity:             %s%s\n", nsid, align,
+           lbacap_to_str(buf, id_ns.ncap, fmt_lba_bits));
+    if (show_all || id_ns.nuse != id_ns.ncap)
+      pout("Namespace %u Utilization:          %s%s\n", nsid, align,
+           lbacap_to_str(buf, id_ns.nuse, fmt_lba_bits));
+
+    // Get LBA range
+    int min_lba_bits = id_ns.lbaf[0].ds, max_lba_bits = min_lba_bits;
+    for (int i = 1; i <= id_ns.nlbaf /* 1-based */ && i < 16; i++) {
+      int bits = id_ns.lbaf[i].ds;
+      if (min_lba_bits > bits)
+        min_lba_bits = bits;
+      else if (max_lba_bits < bits)
+        max_lba_bits = bits;
+    }
+    if (show_all || min_lba_bits != max_lba_bits)
+      pout("Namespace %u Formatted LBA Size:   %s%d (supported: %d - %d)\n", nsid, align,
+           (1 << fmt_lba_bits), (1 << min_lba_bits), (1 << max_lba_bits));
+    else
+      pout("Namespace %u Formatted LBA Size:   %s%d\n", nsid, align, (1 << fmt_lba_bits));
+  }
 
   char td[DATEANDEPOCHLEN]; dateandtimezone(td);
   pout("Local Time is:                      %s\n", td);
@@ -127,7 +220,8 @@ static void print_critical_warning(unsigned char w)
   pout("\n");
 }
 
-static void print_smart_log(const nvme_smart_log & smart_log, unsigned nsid)
+static void print_smart_log(const nvme_smart_log & smart_log, unsigned nsid,
+  const nvme_id_ctrl & id_ctrl, bool show_all)
 {
   char buf[64];
   pout("SMART/Health Information (NVMe Log 0x02, NSID 0x%x)\n", nsid);
@@ -147,11 +241,16 @@ static void print_smart_log(const nvme_smart_log & smart_log, unsigned nsid)
   pout("Unsafe Shutdowns:                   %s\n", le128_to_str(buf, smart_log.unsafe_shutdowns));
   pout("Media and Data Integrity Errors:    %s\n", le128_to_str(buf, smart_log.media_errors));
   pout("Error Information Log Entries:      %s\n", le128_to_str(buf, smart_log.num_err_log_entries));
-  pout("Warning  Comp. Temperature Time:    %d\n", smart_log.warning_temp_time);
-  pout("Critical Comp. Temperature Time:    %d\n", smart_log.critical_comp_time);
-  // Print supported sensors only
+
+  // Temperature thresholds are optional
+  if (show_all || id_ctrl.wctemp || smart_log.warning_temp_time)
+    pout("Warning  Comp. Temperature Time:    %d\n", smart_log.warning_temp_time);
+  if (show_all || id_ctrl.cctemp || smart_log.critical_comp_time)
+    pout("Critical Comp. Temperature Time:    %d\n", smart_log.critical_comp_time);
+
+  // Temperature sensors are optional
   for (int i = 0; i < 8; i++) {
-    if (smart_log.temp_sensor[i])
+    if (show_all || smart_log.temp_sensor[i])
       pout("Temperature Sensor %d:               %s\n", i + 1,
            kelvin_to_str(buf, smart_log.temp_sensor[i]));
   }
@@ -203,6 +302,9 @@ int nvmePrintMain(nvme_device * device, const nvme_print_options & options)
      return 0;
    }
 
+  // Show unset optional values only if debugging is enabled
+  bool show_all = (nvme_debugmode > 0);
+
   // Read Identify Controller always
   nvme_id_ctrl id_ctrl;
   if (!nvme_read_id_ctrl(device, id_ctrl)) {
@@ -210,10 +312,30 @@ int nvmePrintMain(nvme_device * device, const nvme_print_options & options)
     return FAILID;
   }
 
-  // Print Identify Controller
+  // Print Identify Controller/Namespace info
   if (options.drive_info) {
     pout("=== START OF INFORMATION SECTION ===\n");
-    print_id_ctrl(id_ctrl);
+    nvme_id_ns id_ns; memset(&id_ns, 0, sizeof(id_ns));
+
+    unsigned nsid = device->get_nsid();
+    if (nsid == 0xffffffffU) {
+      // Broadcast namespace
+      if (id_ctrl.nn == 1) {
+        // No namespace management, get size from single namespace
+        nsid = 1;
+        if (!nvme_read_id_ns(device, nsid, id_ns))
+          nsid = 0;
+      }
+    }
+    else {
+        // Identify current namespace
+        if (!nvme_read_id_ns(device, nsid, id_ns)) {
+          pout("Read NVMe Identify Namespace 0x%x failed: %s\n", nsid, device->get_errmsg());
+          return FAILID;
+        }
+    }
+
+    print_id_ctrl_ns(id_ctrl, id_ns, nsid, show_all);
   }
 
   if (   options.smart_check_status || options.smart_vendor_attrib
@@ -236,7 +358,7 @@ int nvmePrintMain(nvme_device * device, const nvme_print_options & options)
     }
 
     if (options.smart_vendor_attrib) {
-      print_smart_log(smart_log, device->get_nsid());
+      print_smart_log(smart_log, device->get_nsid(), id_ctrl, show_all);
     }
   }
 
