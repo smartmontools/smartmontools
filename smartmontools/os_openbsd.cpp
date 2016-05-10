smartmontools/smartmontools

commit ade34b2d00e221eb034dd60f114f911418af2fd3

fixes suggested by cppcheck: check realloc result to avoid memory leak (memleakOnRealloc), Fix printf() signednsess (invalidPrintfArgType_sint)
git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4156 4ea69e1a-61f1-4043-bf83-b5c94c648137

smartmontools/os_openbsd.cpp
@@ -136,7 +136,14 @@ get_dev_names(char ***names, const char *prefix)
     n++;
   }
 
-  mp = (char **)realloc(mp, n * (sizeof(char *)));
+  void * tmp = (char **)realloc(mp, n * (sizeof(char *)));
+  if (NULL == tmp) {
+    pout("Out of memory constructing scan device list\n");
+    free(mp);
+    return -1;
+  }
+  else
+    mp = tmp;
   bytes += (n) * (sizeof(char *));
   *names = mp;
   return n;

smartmontools/cciss.cpp
@@ -180,7 +180,7 @@ static int cciss_sendpassthru(unsigned int cmdtype, unsigned char *CDB,
 
     if ((err = ioctl(fd, CCISS_PASSTHRU, &iocommand))) 
     {
-        fprintf(stderr, "CCISS ioctl error %d (fd %d CDBLen %d buf_size %d)\n",
+        fprintf(stderr, "CCISS ioctl error %d (fd %d CDBLen %u buf_size %u)\n",
 	    fd, err, CDBlen, size);
     }
     return err;

smartmontools/os_linux.cpp
@@ -2830,7 +2830,7 @@ bool linux_smart_interface::get_dev_megasas(smart_device_list & devlist)
         continue;
       /* proc_name should be megaraid_sas */
       snprintf(sysfsdir, sizeof(sysfsdir) - 1,
-        "/sys/class/scsi_host/host%d/proc_name", host_no);
+        "/sys/class/scsi_host/host%u/proc_name", host_no);
       if((fp = fopen(sysfsdir, "r")) == NULL)
         continue;
       if(fgets(line, sizeof(line), fp) != NULL && !strncmp(line,"megaraid_sas",12)) {

smartmontools/os_netbsd.cpp
@@ -132,7 +132,14 @@ get_dev_names(char ***names, const char *prefix)
     n++;
   }
 
-  mp = (char **)realloc(mp, n * (sizeof(char *)));
+  void * tmp = (char **)realloc(mp, n * (sizeof(char *)));
+  if (NULL == tmp) {
+    pout("Out of memory constructing scan device list\n");
+    free(mp);
+    return -1;
+  }
+  else
+    mp = tmp;
   bytes += (n) * (sizeof(char *));
   *names = mp;
   return n;

smartmontools/os_win32.cpp
@@ -1043,7 +1043,7 @@ bool win_smart_interface::scan_smart_devices(smart_device_list & devlist,
             raid_seen[vers_ex.wControllerId] = true;
             // Add physical drives
             int len = strlen(name);
-            for (int pi = 0; pi < 32; pi++) {
+            for (unsigned int pi = 0; pi < 32; pi++) {
               if (vers_ex.dwDeviceMapEx & (1L << pi)) {
                 snprintf(name+len, sizeof(name)-1-len, ",%u", pi);
                 devlist.push_back( new win_ata_device(this, name, "ata") );

commit 1ab2dfba39b75bd600046606bf0a1a5fc148add4

os_openbsd.cpp: Compile fix (regression from r4156).
git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4321 4ea69e1a-61f1-4043-bf83-b5c94c648137

smartmontools/os_openbsd.cpp
@@ -132,7 +132,7 @@ get_dev_names(char ***names, const char *prefix)
     n++;
   }
 
-  void * tmp = (char **)realloc(mp, n * (sizeof(char *)));
+  char ** tmp = (char **)realloc(mp, n * (sizeof(char *)));
   if (NULL == tmp) {
     pout("Out of memory constructing scan device list\n");
     free(mp);