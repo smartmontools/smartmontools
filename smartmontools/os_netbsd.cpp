0fbad301510356d11e2ee13bc22b3382c645e222
chrfranke <authors@smartmontools.org>
2016-05-10 13:39:19 +0000

    os_netbsd.cpp: Apply patch-os__netbsd.cpp 1.3 (2016-05-08) from pkgsrc.se/sysutils/smartmontools: - Compile fix (regression from r4156). - Use a raw disk device file on NetBSD.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4320 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_netbsd.cpp
+++ smartmontools/os_netbsd.cpp
@@ -54,7 +54,7 @@ printwarning(int msgNo, const char *extra)
   return;
 }
 
-static const char *net_dev_prefix = "/dev/";
+static const char *net_dev_prefix = "/dev/r";
 static const char *net_dev_ata_disk = "wd";
 static const char *net_dev_scsi_disk = "sd";
 static const char *net_dev_scsi_tape = "enrst";
@@ -128,7 +128,7 @@ get_dev_names(char ***names, const char *prefix)
     n++;
   }
 
-  void * tmp = (char **)realloc(mp, n * (sizeof(char *)));
+  char ** tmp = (char **)realloc(mp, n * (sizeof(char *)));
   if (NULL == tmp) {
     pout("Out of memory constructing scan device list\n");
     free(mp);
