831cd26323a19e107fbc38705acd76ddd3647ec0
Christian Franke <christian.franke@t-online.de>
2016-10-23 15:48:09 +0000

    configure.ac, os_netbsp.cpp: Add --with-nvme-devicescan for NetBSD.
    svn-path: trunk@4354

--- smartmontools/os_netbsd.cpp
+++ smartmontools/os_netbsd.cpp
@@ -32,7 +32,7 @@
 // based on "sys/dev/ic/nvmeio.h" from NetBSD kernel sources
 #include "netbsd_nvme_ioctl.h" // NVME_PASSTHROUGH_CMD, nvme_completion_is_error
 
-const char * os_netbsd_cpp_cvsid = "$Id: os_netbsd.cpp 4343 2016-09-25 16:46:49Z chrfranke $"
+const char * os_netbsd_cpp_cvsid = "$Id: os_netbsd.cpp 4354 2016-10-23 15:48:09Z chrfranke $"
   OS_NETBSD_H_CVSID;
 
 enum warnings {
@@ -750,7 +750,12 @@ bool netbsd_smart_interface::scan_smart_devices(smart_device_list & devlist,
 
   bool scan_ata = !*type || !strcmp(type, "ata");
   bool scan_scsi = !*type || !strcmp(type, "scsi") || !strcmp(type, "sat");
+
+#ifdef WITH_NVME_DEVICESCAN // TODO: Remove when NVMe support is no longer EXPERIMENTAL
   bool scan_nvme = !*type || !strcmp(type, "nvme");
+#else
+  bool scan_nvme =           !strcmp(type, "nvme");
+#endif
 
   // Make namelists
   char * * atanames = 0; int numata = 0;
