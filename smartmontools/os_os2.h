3a3003de9d5d098b074b9102abf07d60551834c8
Alex Samorukov <samm@net-art.cz>
2017-04-19 08:50:03 +0000

    Many OS/2 related fixes
    svn-path: trunk@4421

--- smartmontools/os_os2.h
+++ smartmontools/os_os2.h
@@ -18,7 +18,7 @@
 #ifndef OS_OS2_H_
 #define OS_OS2_H_
 
-#define OS_XXXX_H_CVSID "$Id: os_os2.h 4120 2015-08-27 16:12:21Z samm2 $\n"
+#define OS_XXXX_H_CVSID "$Id: os_os2.h 4421 2017-04-19 08:50:03Z samm2 $\n"
 
 // Additional material should start here.  Note: to keep the '-V' CVS
 // reporting option working as intended, you should only #include
@@ -28,7 +28,6 @@
 #define INCL_DOS
 #include <os2.h>
 
-#include "os_os2\hdreg.h"
 #include "os_linux.h"
 
 #pragma pack(1)
@@ -40,10 +39,10 @@
 #define DSKSP_SMART_GETSTATUS       0x23  /* get SMART status (pass/fail) */
 #define DSKSP_SMART_GET_ATTRIBUTES  0x24  /* get SMART attributes table */
 #define DSKSP_SMART_GET_THRESHOLDS  0x25  /* get SMART thresholds table */
-#define DSKSP_SMART_READ_LOG        0x26  
-#define DSKSP_SMART_WRITE_LOG       0x27  
-#define DSKSP_SMART_READ_LOG_EXT    0x28  
-#define DSKSP_SMART_WRITE_LOG_EXT   0x29  
+#define DSKSP_SMART_READ_LOG        0x26
+#define DSKSP_SMART_WRITE_LOG       0x27
+#define DSKSP_SMART_READ_LOG_EXT    0x28
+#define DSKSP_SMART_WRITE_LOG_EXT   0x29
 #define DSKSP_SMART_EOLI            0x30  /* EXECUTE OFF-LINE IMMEDIATE */
 
 #define SMART_CMD_ON      1   /* on  value for related SMART functions */
