9e38757875fdea72308b2efd53d87c01d7e6e079
samm-git <samm@os2.kiev.ua>
2017-09-23 13:20:06 +0000

    drivedb.h: Added Transcend SSD230 drives (#879)
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4472 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/drivedb.h
+++ smartmontools/drivedb.h
@@ -1497,6 +1497,7 @@ const drive_settings builtin_knowndrives[] = {
       // TS256GMTS400
     "TS(16|32|64|128|512|256)GMTS400S?|"
     "TS(120|240)GMTS420|" // Transcend MTS420, TS(120|240)GMTS420
+    "TS(128G|256G|512G|1T)SSD230S|" // TS128GSSD230S/P1025F8
     "ADATA SP550", // ADATA SP550/O0803B5a
     "", "",
   //"-v 1,raw48,Raw_Read_Error_Rate "
