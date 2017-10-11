smartmontools/smartmontools

commit ce8599486e179df98ab5f4a1075a6550ed78d9e2

configure.ac, os_darwin.cpp: Align Darwin NVMe device scanning with other platforms: Disable unless '--with-nvme-devicescan' or '-d nvme' is specified.  Print related configure warning.
git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4549 4ea69e1a-61f1-4043-bf83-b5c94c648137

smartmontools/os_darwin.cpp
@@ -717,7 +717,11 @@ bool darwin_smart_interface::scan_smart_devices(smart_device_list & devlist,
     }
   }
   char * * nvmenames = 0; int numnvme = 0;
-  if (!type || !strcmp(type, "NVME")) {
+  if (
+#ifdef WITH_NVME_DEVICESCAN // TODO: Remove when NVMe support is no longer EXPERIMENTAL
+      !type ||
+#endif
+               !strcmp(type, "nvme")) {
     numnvme = make_device_names(&nvmenames, "NVME");
     if (numnvme < 0) {
       set_err(ENOMEM);

smartmontools/configure.ac
@@ -815,7 +815,8 @@ info=`
         linux*) echo "SELinux support:        ${with_selinux-no}" ;;
       esac
       case "$host_os" in
-        linux*|netbsd*|cygwin*) echo "NVMe DEVICESCAN:        ${with_nvme_devicescan-no}" ;;
+        linux*|darwin*|netbsd*|cygwin*)
+          echo "NVMe DEVICESCAN:        ${with_nvme_devicescan-no}" ;;
       esac
       ;;
   esac
@@ -828,7 +829,7 @@ $info
 
 # TODO: Remove when NVMe support is no longer EXPERIMENTAL
 case "$host_os:${with_nvme_devicescan+set}" in
-  linux*:|netbsd*:|cygwin*:|mingw*:)
+  linux*:|darwin*:|netbsd*:|cygwin*:|mingw*:)
     AC_MSG_WARN([
 This version of smartmontools provides NVMe support which is still
 EXPERIMENTAL.  NVMe devices are not yet included in smartd.conf

commit 2fb5c6b0cb96653c39fd4aac5103ea9b68e0910e

os_darwin.cpp: fix crash on --scan (regression from r4549)
svn-path: trunk@4552

smartmontools/os_darwin.cpp
@@ -45,7 +45,7 @@
 
 #define ARGUSED(x) ((void)(x))
 // Needed by '-V' option (CVS versioning) of smartd/smartctl
-const char *os_darwin_cpp_cvsid="$Id: os_darwin.cpp 4549 2017-10-10 19:09:54Z chrfranke $" \
+const char *os_darwin_cpp_cvsid="$Id: os_darwin.cpp 4552 2017-10-11 10:11:35Z samm2 $" \
 ATACMDS_H_CVSID CONFIG_H_CVSID INT64_H_CVSID OS_DARWIN_H_CVSID SCSICMDS_H_CVSID UTILITY_H_CVSID;
 
 // examples for smartctl
@@ -78,7 +78,7 @@ static struct {
   IONVMeSMARTInterface **smartIfNVMe;
 } devices[20];
 
-const char * dev_darwin_cpp_cvsid = "$Id: os_darwin.cpp 4549 2017-10-10 19:09:54Z chrfranke $"
+const char * dev_darwin_cpp_cvsid = "$Id: os_darwin.cpp 4552 2017-10-11 10:11:35Z samm2 $"
   DEV_INTERFACE_H_CVSID;
 
 /////////////////////////////////////////////////////////////////////////////
@@ -720,6 +720,8 @@ bool darwin_smart_interface::scan_smart_devices(smart_device_list & devlist,
   if (
 #ifdef WITH_NVME_DEVICESCAN // TODO: Remove when NVMe support is no longer EXPERIMENTAL
       !type ||
+#else
+      type &&
 #endif
                !strcmp(type, "nvme")) {
     numnvme = make_device_names(&nvmenames, "NVME");