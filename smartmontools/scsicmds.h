cd3d87955f9429c4830706b7176284c164d19e99
Alex Samorukov <samm@net-art.cz>
2017-10-19 19:17:29 +0000

    scsicmds.h: increase SCSI_TIMEOUT_DEFAULT to 1 minute to work on the big JBOD arrays (#917)
    svn-path: trunk@4557

--- smartmontools/scsicmds.h
+++ smartmontools/scsicmds.h
@@ -30,7 +30,7 @@
 #ifndef SCSICMDS_H_
 #define SCSICMDS_H_
 
-#define SCSICMDS_H_CVSID "$Id: scsicmds.h 4152 2015-10-17 16:08:21Z chrfranke $\n"
+#define SCSICMDS_H_CVSID "$Id: scsicmds.h 4557 2017-10-19 19:17:29Z samm2 $\n"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -304,8 +304,9 @@ Documentation, see http://www.storage.ibm.com/techsup/hddtech/prodspecs.htm */
 
 
 /* SCSI command timeout values (units are seconds) */
-#define SCSI_TIMEOUT_DEFAULT    20  // should be longer than the spin up time
-                                    // of a disk in standby mode.
+#define SCSI_TIMEOUT_DEFAULT    60  // should be longer than the spin up time
+                                    // of a disk in JBOD.
+
 #define SCSI_TIMEOUT_SELF_TEST  (5 * 60 * 60)   /* allow max 5 hours for */
                                             /* extended foreground self test */
 
