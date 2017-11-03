c6c19d38d8fc1346497d9d5f0389309ef1ad3b05
chrfranke <authors@smartmontools.org>
2017-11-03 19:58:02 +0000

    os_linux.cpp: Fix indentation (g++ 6.3: -Wmisleading-indentation).
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4581 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_linux.cpp
+++ smartmontools/os_linux.cpp
@@ -1052,7 +1052,7 @@ bool linux_aacraid_device::scsi_pass_through(scsi_cmnd_io *iop)
     j  = snprintf(buff, sz, " [%s: ", np ? np : "<unknown opcode>");
     for (k = 0; k < (int)iop->cmnd_len; ++k)
       j += snprintf(&buff[j], (sz > j ? (sz - j) : 0), "%02x ", ucp[k]);
-      if ((report > 1) &&
+    if ((report > 1) &&
         (DXFER_TO_DEVICE == iop->dxfer_dir) && (iop->dxferp)) {
         int trunc = (iop->dxfer_len > 256) ? 1 : 0;
 
