d51ed9dadafb67ec08ec05380729d20381f00947
samm-git <samm@os2.kiev.ua>
2017-11-05 22:05:37 +0000

    atacmds.h: avoid freebsd warning related to ATA_WRITE_LOG_EXT redefine
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4598 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/atacmds.h
+++ smartmontools/atacmds.h
@@ -93,7 +93,7 @@ typedef enum {
 
 // 48-bit commands
 #define ATA_READ_LOG_EXT                0x2F
-#define ATA_WRITE_LOG_EXT               0x3F
+#define ATA_WRITE_LOG_EXT               0x3f
 
 // ATA Specification Feature Register Values (SMART Subcommands).
 // Note that some are obsolete as of ATA-7.
