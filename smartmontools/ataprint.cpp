3a996efd6e69e1e0379fbbf27e09f6b24d3da0e9
Christian Franke <christian.franke@t-online.de>
2017-11-08 06:30:44 +0000

    ataprint.cpp: Fix detection of Device Statistics log with 256 sectors (#922).
    svn-path: trunk@4604

--- smartmontools/ataprint.cpp
+++ smartmontools/ataprint.cpp
@@ -40,7 +40,7 @@
 #include "utility.h"
 #include "knowndrives.h"
 
-const char * ataprint_cpp_cvsid = "$Id: ataprint.cpp 4573 2017-10-29 15:13:58Z chrfranke $"
+const char * ataprint_cpp_cvsid = "$Id: ataprint.cpp 4604 2017-11-08 06:30:44Z chrfranke $"
                                   ATAPRINT_H_CVSID;
 
 
@@ -3687,7 +3687,7 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
     bool use_gplog = true;
     unsigned nsectors = 0;
     if (gplogdir) 
-      nsectors = GetNumLogSectors(gplogdir, 0x04, false);
+      nsectors = GetNumLogSectors(gplogdir, 0x04, true);
     else if (smartlogdir){ // for systems without ATA_READ_LOG_EXT
       nsectors = GetNumLogSectors(smartlogdir, 0x04, false);
       use_gplog = false;
