3e21a4dcfcb6c766e931a11ef40409b72b85d34a
Christian Franke <christian.franke@t-online.de>
2018-01-01 10:30:47 +0000

    Happy New Year! Update copyright year in version info.
    svn-path: trunk@4680

--- smartmontools/utility.cpp
+++ smartmontools/utility.cpp
@@ -4,7 +4,7 @@
  * Home page of code is: http://www.smartmontools.org
  *
  * Copyright (C) 2002-12 Bruce Allen
- * Copyright (C) 2008-17 Christian Franke
+ * Copyright (C) 2008-18 Christian Franke
  * Copyright (C) 2000 Michael Cornwell <cornwell@acm.org>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -52,7 +52,7 @@
 #include "atacmds.h"
 #include "dev_interface.h"
 
-const char * utility_cpp_cvsid = "$Id: utility.cpp 4583 2017-11-03 21:10:09Z chrfranke $"
+const char * utility_cpp_cvsid = "$Id: utility.cpp 4680 2018-01-01 10:30:47Z chrfranke $"
                                  UTILITY_H_CVSID INT64_H_CVSID;
 
 const char * packet_types[] = {
@@ -90,7 +90,7 @@ std::string format_version_info(const char * prog_name, bool full /*= false*/)
       "(build date " __DATE__ ")" // checkout without expansion of Id keywords
 #endif
       " [%s] " BUILD_INFO "\n"
-    "Copyright (C) 2002-17, Bruce Allen, Christian Franke, www.smartmontools.org\n",
+    "Copyright (C) 2002-18, Bruce Allen, Christian Franke, www.smartmontools.org\n",
     prog_name, smi()->get_os_version_str().c_str()
   );
   if (!full)
