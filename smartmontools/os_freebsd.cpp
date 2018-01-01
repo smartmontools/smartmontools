ef90b4aacaa104e2c8df9342cb31fbed7ece8ee2
samm-git <samm@os2.kiev.ua>
2018-01-01 21:11:52 +0000

    os_freebsd.cpp: fix build with CLANG/6. Patch provided by Dimitry Andric, PR 224826
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4681 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_freebsd.cpp
+++ smartmontools/os_freebsd.cpp
@@ -484,7 +484,7 @@ bool freebsd_nvme_device::open()
   	}
   	nsid = 0xFFFFFFFF; // broadcast id
   }
-  else if (sscanf(dev, NVME_CTRLR_PREFIX"%d"NVME_NS_PREFIX"%d%c", 
+  else if (sscanf(dev, NVME_CTRLR_PREFIX"%d" NVME_NS_PREFIX "%d%c", 
   	&ctrlid, &nsid, &tmp) == 2) 
   {
   	if(ctrlid < 0 || nsid < 0) {
