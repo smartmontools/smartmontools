smartmontools/smartmontools

commit d2131ab910c5a8016b511f5c23df43b157a62958

os_linux.cpp: Use 'realpath()' (BSD, POSIX) instead of 'canonicalize_file_name()' (GNU extension). This fixes build on systems with musl libc (#921).
git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4603 4ea69e1a-61f1-4043-bf83-b5c94c648137

smartmontools/os_linux.cpp
@@ -3176,7 +3176,7 @@ static bool is_hpsa(const char * name)
 {
   char path[128];
   snprintf(path, sizeof(path), "/sys/block/%s/device", name);
-  char * syshostpath = canonicalize_file_name(path);
+  char * syshostpath = realpath(name, (char *)0);
   if (!syshostpath)
     return false;
 

commit 0e15a0fcf41eb7c913cc0c1e6d9eece01a1ce6b9

os_linux.cpp: Call realpath() with full /sys/* path instead of device name (GH pull/23).  This fixes detection of hpsa devices (regression from r4603).
svn-path: trunk@4854

smartmontools/os_linux.cpp
@@ -94,7 +94,7 @@
 
 #define ARGUSED(x) ((void)(x))
 
-const char * os_linux_cpp_cvsid = "$Id: os_linux.cpp 4848 2018-12-05 18:30:46Z chrfranke $"
+const char * os_linux_cpp_cvsid = "$Id: os_linux.cpp 4854 2018-12-11 20:32:29Z chrfranke $"
   OS_LINUX_H_CVSID;
 extern unsigned char failuretest_permissive;
 
@@ -3209,7 +3209,7 @@ static bool is_hpsa(const char * name)
 {
   char path[128];
   snprintf(path, sizeof(path), "/sys/block/%s/device", name);
-  char * syshostpath = realpath(name, (char *)0);
+  char * syshostpath = realpath(path, (char *)0);
   if (!syshostpath)
     return false;
 