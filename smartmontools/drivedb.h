77cb65d03828bc2b76caa78dfb7a421537343c0b
chrfranke <authors@smartmontools.org>
2019-04-22 15:13:40 +0000

    drivedb.h: - USB: JMicron JMS578 (0x0080:0x0578) - USB: Unknown (0x0080:0xa0001) (#852)
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4909 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/drivedb.h
+++ smartmontools/drivedb.h
@@ -4405,6 +4405,19 @@ const drive_settings builtin_knowndrives[] = {
   // USB ID entries
   ////////////////////////////////////////////////////
 
+  // 0x0080 (JMicron/Toshiba ?)
+  { "USB: ; JMicron JMS578",
+    "0x0080:0x0578",
+    "", // 0x0104
+    "",
+    "-d sat"
+  },
+  { "USB: ; ",
+    "0x0080:0xa001",
+    "", // ORICO 2588US3: 0x0101, 0x0203
+    "",
+    "-d sat"
+  },
   // 0x0350 (?)
   { "USB: ViPowER USB3.0 Storage; ",
     "0x0350:0x0038",
