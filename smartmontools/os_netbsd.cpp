b279c91c8b01e48f50df4ceb4def064311d54e7b
chrfranke <authors@smartmontools.org>
2019-06-12 20:29:55 +0000

    os_netbsd.cpp: Fix device scan crash on empty name list. Fix a memory leak introduced 15 years ago in r1434.
    Thanks to Alexander Nasonov for bug report and testing.
    
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4919 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_netbsd.cpp
+++ smartmontools/os_netbsd.cpp
@@ -602,6 +602,12 @@ int netbsd_smart_interface::get_dev_names(char ***names, const char *prefix)
     sprintf(mp[n], "%s%s%c", net_dev_raw_prefix, p, 'a' + getrawpartition());
     n++;
   }
+  free(disknames);
+
+  if (n == 0) {
+    free(mp);
+    return 0;
+  }
 
   char ** tmp = (char **)realloc(mp, n * (sizeof(char *)));
   if (NULL == tmp) {
