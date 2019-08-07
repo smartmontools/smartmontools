9b8bcb513ffd2dfaea609ec72b14d69c5aea68c1
chrfranke <authors@smartmontools.org>
2019-08-07 21:02:45 +0000

    os_win32/wtssendmsg.c: Add '-t' and '-w' option.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@4940 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_win32/wtssendmsg.c
+++ smartmontools/os_win32/wtssendmsg.c
@@ -8,12 +8,13 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-#define WINVER 0x0500
+#define WINVER 0x0501
 #define _WIN32_WINNT WINVER
 
 char svnid[] = "$Id$";
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define WIN32_LEAN_AND_MEAN
@@ -25,35 +26,61 @@ static int usage()
 {
   printf("wtssendmsg $Revision$ - Display a message box on client desktops\n"
          "Copyright (C) 2012-19 Christian Franke, www.smartmontools.org\n\n"
-         "Usage: wtssendmsg [-cas] [-v] [\"Caption\"] \"Message\"|-\n"
+         "Usage: wtssendmsg [-cas] [-t TIMEOUT] [-w 0..5] [-v] [\"Caption\"] \"Message\"|-\n"
          "       wtssendmsg -v\n\n"
          "  -c    Console session [default]\n"
          "  -a    Active sessions\n"
          "  -s    Connected sessions\n"
+         "  -t    Remove message box after TIMEOUT seconds\n"
+         "  -w    Select buttons and wait for response or timeout\n"
          "  -v    List sessions\n"
   );
   return 1;
 }
 
+static int getnum(const char * s)
+{
+  char * endp;
+  int n = strtol(s, &endp, 10);
+  if (*endp)
+    return -1;
+  return n;
+}
+
 int main(int argc, const char **argv)
 {
-  int mode = 0, verbose = 0, status = 0, i;
-  const char * message = 0, * caption = "";
-  char msgbuf[1024];
-  WTS_SESSION_INFOA * sessions; DWORD count;
+  int mode = 0, timeout = 0, buttons = -1, verbose = 0, i;
 
   for (i = 1; i < argc && argv[i][0] == '-' && argv[i][1]; i++) {
     int j;
-    for (j = 1; argv[i][j]; j++)
+    for (j = 1; j > 0 && argv[i][j]; j++)
       switch (argv[i][j]) {
-      case 'c': mode = 0; break;
-      case 'a': mode = 1; break;
-      case 's': mode = 2; break;
-      case 'v': verbose = 1; break;
-      default: return usage();
-    }
+        case 'c': mode = 0; break;
+        case 'a': mode = 1; break;
+        case 's': mode = 2; break;
+        case 't':
+          if (argv[i][j+1] || ++i >= argc)
+            return usage();
+          timeout = getnum(argv[i]);
+          if (timeout < 0)
+            return usage();
+          j = 0;
+          break;
+        case 'w':
+          if (argv[i][j+1] || ++i >= argc)
+            return usage();
+          buttons = getnum(argv[i]);
+          if (!(MB_OK <= buttons && buttons <= MB_RETRYCANCEL)) // 0..5
+            return usage();
+          j = 0;
+          break;
+        case 'v': verbose = 1; break;
+        default: return usage();
+      }
   }
 
+  const char * message = 0, * caption = "";
+  char msgbuf[1024];
   if (i < argc) {
     if (i+1 < argc)
       caption = argv[i++];
@@ -82,11 +109,13 @@ int main(int argc, const char **argv)
   }
 
   // Get session list
+  WTS_SESSION_INFOA * sessions; DWORD count;
   if (!WTSEnumerateSessionsA(WTS_CURRENT_SERVER_HANDLE, 0, 1, &sessions, &count)) {
     fprintf(stderr, "WTSEnumerateSessions() failed\n");
     return 1;
   }
 
+  int status = 0;
   for (i = 0; i < (int)count; i++) {
 
     if (verbose) {
@@ -98,32 +127,48 @@ int main(int argc, const char **argv)
       fflush(stdout);
     }
 
-    if (   !strcmpi(sessions[i].pWinStationName, "Console")
-        || (mode >= 1 && sessions[i].State == WTSActive)
-        || (mode >= 2 && sessions[i].State == WTSConnected)) {
-
-      // Send Message, don't wait for OK button
-      DWORD result;
-      if (WTSSendMessageA(WTS_CURRENT_SERVER_HANDLE, sessions[i].SessionId,
-          (char *)caption, strlen(caption),
-          (char *)message, strlen(message),
-          MB_OK|MB_ICONEXCLAMATION, 0 /*Timeout*/,
-          &result, FALSE /*!Wait*/)) {
-        if (verbose)
-          printf("message sent\n");
-      }
-      else {
-        status = 1;
-        if (verbose)
-          printf("WTSSendMessage() failed with error=%d\n", (int)GetLastError());
-        else
-          fprintf(stderr, "Session %d (\"%s\", State=%d): WTSSendMessage() failed with error=%d\n",
-                  i, sessions[i].pWinStationName, sessions[i].State, (int)GetLastError());
-      }
-    }
-    else {
+    // Check session state
+    if (!(   !strcmpi(sessions[i].pWinStationName, "Console")
+          || (mode >= 1 && sessions[i].State == WTSActive)
+          || (mode >= 2 && sessions[i].State == WTSConnected))) {
       if (verbose)
         printf("ignored\n");
+      continue;
+    }
+
+    // Send Message
+    DWORD response = ~0;
+    if (!WTSSendMessageA(WTS_CURRENT_SERVER_HANDLE, sessions[i].SessionId,
+                         (char *)caption, strlen(caption),
+                         (char *)message, strlen(message),
+                         (buttons <= MB_OK ? MB_OK|MB_ICONEXCLAMATION
+                          : buttons|MB_DEFBUTTON2|MB_ICONQUESTION    ),
+                         timeout, &response, (buttons >= MB_OK) /*Wait?*/ )) {
+      status |= 0x01;
+      if (verbose)
+        printf("WTSSendMessage() failed with error=%d\n", (int)GetLastError());
+      else
+        fprintf(stderr, "Session %d (\"%s\", State=%d): WTSSendMessage() failed with error=%d\n",
+                i, sessions[i].pWinStationName, sessions[i].State, (int)GetLastError());
+      continue;
+    }
+
+    if (buttons >= MB_OK) {
+      switch (response) {
+        case IDOK:
+        case IDYES:    case IDABORT:   status |= 0x02; break;
+        case IDNO:     case IDRETRY:   status |= 0x04; break;
+        case IDCANCEL: case IDIGNORE:  status |= 0x08; break;
+        case IDTIMEOUT:                status |= 0x10; break;
+        default:                       status |= 0x01; break;
+      }
+      if (verbose)
+        printf("response = %d, status = 0x%02x\n", (int)response, status);
+    }
+    else {
+      // response == IDASYNC
+      if (verbose)
+        printf("message sent\n");
     }
   }
 
