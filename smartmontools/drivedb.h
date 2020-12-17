0ab1df98d184d2b93e38fa96bd6f28e49c0dbcc0
Johnny Nguyen <johnnynguyen541@gmail.com>
2020-12-17 09:40:28 -0800

    Virtium Drive Entry for DriveDB.h - StorFly SATA 6Gbps SSDs

--- smartmontools/drivedb.h
+++ smartmontools/drivedb.h
@@ -711,165 +711,15 @@ const drive_settings builtin_knowndrives[] = {
     "-v 249,raw48,Spares_Remaining_Perc " //  later then 0409 FW.
   },
   {
-    "StorFly TuffDrive eUSB", 
-    ".*VTDU31XI008G-A901.*",
-    // X - XE Class
-    // I - Industrial
-    "", "",
-    "-v 12,raw48,Power_Cycle_Count "
-    "-v 196,raw48,Reallocated_Event_Count "
-    "-v 199,raw48,UDMA_CRC_Error_Count "
-    "-v 203,raw48,Total_ECC_Errors "
-    "-v 204,raw48,Corrected_ECC_Error "
-    "-v 213,raw48,Spare_Worst_Threshold "
-    "-v 214,raw48,Anchor_Block_Status "
-    "-v 215,raw48,Trim_Status "
-    "-v 229,raw48,Erase_Count "
-    "-v 232,raw48,Total_Number_of_Reads "
-    "-v 241,raw48,Total_LBAs_Written "
-    "-v 242,raw48,Total_LBAs_Read "
-  },
-  {
-    "StorFly SATA 6Gbps pSLC SSD", 
-    ".*VSF205[XC][CI](050|100)G-A902.*|"  // SlimSATA
-    ".*VSFDM[4|6|8][XC][CI](050|100)G-A902.*",  // M.2
-    // X - XE Class, C - CE Class
-    // C - commercial, I industrial
-    "0705-000", "",
-    "-v 1,raw48,Raw_Read_Error_Rate " //
-    "-v 5,raw48,Reallocated_Blk_Cnt "
-    "-v 9,raw24(raw8),Pwr_On_Hours "
-    "-v 12,raw48,Pwr_Cycle_Cnt "
-    "-v 14,raw48,Device_Raw_Capacity "
-    "-v 15,raw48,Device_User_Capacity "
-    "-v 16,raw48,pSLC_Spare_Blk_Cnt "
-    "-v 17,raw48,pSLC_Spare_Blk_Left "
-    "-v 100,raw48,Total_pSLC_Erase_Cnt "
-    "-v 168,raw48,SATA_PHY_Error_Cnt "
-    "-v 170,raw48,Total_Bad_Blk_Cnt "
-    "-v 172,raw48,Total_Blk_Erase_Failures "
-    "-v 173,raw48,Max_pSLC_PE_Cycle_Cnt "
-    "-v 174,raw48,Unexpected_Pwr_Loss_Cnt "
-    "-v 175,raw48,Average_pSLC_Erase_Cnt "
-    "-v 176,raw48,pSLC_NAND_PE_Endurance "
-    "-v 181,raw48,Total_Program_Fail "
-    "-v 187,raw48,Uncorrectable_Error_Cnt "
-    "-v 194,raw48,Temperature_Celsius "
-    "-v 198,raw48,RAID_Recovery_Cnt "
-    "-v 199,raw48,UDMA_CRC_Error_Cnt "
-    "-v 202,raw48,Drive_Live_Used "
-    "-v 232,raw48,RAID_Trigger_Cnt "
-    "-v 234,raw48,Total_Flash_Sectors_Read "
-    "-v 235,raw48,Total_Flash_Sectors_Wrtn "
-    "-v 241,raw48,Total_LBAs_Wrtn "
-    "-v 242,raw48,Total_LBAs_Read "
-    "-v 248,raw48,Remaining_Life_Left "
-    "-v 249,raw48,Remaining_Spare_Blk_Cnt "
-  },
-  {
-    "StorFly SATA 6Gbps TLC SSD", 
-    ".*VSF205CI100G-A902.*|"  // SlimSATA
-    ".*VSFDM6CC400G-A901.*|"  // M.2
-    ".*VSFDM8CC960G-A901.*|"  // M.2
-    ".*VSFDM8CC100G-A902.*",  // M.2
-    // X - XE Class, C - CE Class
-    // C - commercial, I industrial
-    "", "",
-    "-v 1,raw48,Raw_Read_Error_Rate " //
-    "-v 5,raw48,Reallocated_Blk_Cnt "
-    "-v 9,raw24(raw8),Pwr_On_Hours "
-    "-v 12,raw48,Pwr_Cycle_Cnt "
-    "-v 14,raw48,Device_Raw_Capacity "
-    "-v 15,raw48,Device_User_Capacity "
-    "-v 16,raw48,TLC_Spare_Blk_Cnt "
-    "-v 17,raw48,TLC_Spare_Blk_Left "
-    "-v 100,raw48,Total_TLC_Erase_Cnt "
-    "-v 168,raw48,SATA_PHY_Error_Cnt "
-    "-v 170,raw48,Total_Bad_Blk_Cnt "
-    "-v 172,raw48,Total_Blk_Erase_Failures "
-    "-v 173,raw48,Max_TLC_PE_Cycle_Cnt "
-    "-v 174,raw48,Unexpected_Pwr_Loss_Cnt "
-    "-v 175,raw48,Average_TLC_Erase_Cnt "
-    "-v 181,raw48,Total_Program_Fail "
-    "-v 187,raw48,Uncorrectable_Error_Cnt "
-    "-v 194,raw48,Temperature_Celsius "
-    "-v 198,raw48,RAID_Recovery_Cnt "
-    "-v 199,raw48,UDMA_CRC_Error_Cnt "
-    "-v 202,raw48,Drive_Live_Used "
-    "-v 232,raw48,RAID_Trigger_Cnt "
-    "-v 234,raw48,Total_Flash_Sectors_Read "
-    "-v 235,raw48,Total_Flash_Sectors_Wrtn "
-    "-v 241,raw48,Total_LBAs_Wrtn "
-    "-v 242,raw48,Total_LBAs_Read "
-    "-v 248,raw48,Remaining_Life_Left "
-    "-v 249,raw48,Remaining_Spare_Blk_Cnt "
-  },
-  {
-    "StorFly SATA 6Gbps SSDs", 
-    ".*VSFBM6CC025G-JUN.*|"
-    ".*VSFBM6CC025G-JUN1.*",
-    "0626-000", "",
-    "-v 1,raw24/raw32,Raw_Read_Error_Rate "
-    "-v 5,raw48,Reallocated_Sector_Count "
-    "-v 9,raw24(raw8),Power_On_Hours "
-    "-v 160,raw48,Uncorrectable_Count "
-    "-v 161,raw48,Spares_Remaining "
-    "-v 169,raw48,Remaining_Life_Left "
-    "-v 194,raw48,Temperature "
-    "-v 199,raw48,CRC_Error_Count "
-    "-v 241,raw48,Total_LBAs_Written "
-    "-v 242,raw48,Total_LBAs_Read "
-    "-v 248,raw48,Remaining_Life_Left "
-    "-v 249,raw48,Spare_Blocks_Remaining "
-  },
-  {
-    "StorFly SATA 6Gbps SSDs", 
-    ".*VSFBM6CC400G-JUN.*|"
-    ".*VSFBM6CC400G-JUN1.*",
-    "0629-000", "",
-    "-v 1,raw24/raw32,Raw_Read_Error_Rate "
-    "-v 5,raw48,Reallocated_Sector_Count "
-    "-v 9,raw24(raw8),Power_On_Hours "
-    "-v 12,raw48,Power_Cycle_Count "
-    "-v 16,raw48,Valid_Spare_Blocks "
-    "-v 17,raw48,Remaining_Spare_Blocks "
-    "-v 160,raw48,Uncorrectable_Count "
-    "-v 161,raw48,Spares_Remaining "
-    "-v 163,raw48,Initial_Invalid_Blocks "
-    "-v 164,raw48,Total_Erase_Count "
-    "-v 165,raw48,Maximum_Erase_Count "
-    "-v 166,raw48,Minimum_Erase_Count "
-    "-v 167,raw48,Average_Erase_Count "
-    "-v 168,raw48,NAND_Endurance "
-    "-v 177,raw48,Total_Wear_Level_Count "
-    "-v 178,raw48,Runtime_Invalid_Blocks "
-    "-v 181,raw48,Total_Program_Fail "
-    "-v 182,raw48,Total_Erase_Fail "
-    "-v 192,raw48,Unsafe_Power_Down "
-    "-v 194,raw48,Temperature "
-    "-v 195,raw48,Hardware_ECC_Recovered "
-    "-v 196,raw48,Reallocation_Event_Count "
-    "-v 198,raw48,Offline_Uncorrectable "
-    "-v 199,raw48,CRC_Error_Count "
-    "-v 231,raw48,Remaining_Life_Left "
-    "-v 232,raw48,Available_Reserve_Space "
-    "-v 241,raw48,Total_LBAs_Written "
-    "-v 242,raw48,Total_LBAs_Read "
-    "-v 248,raw48,Remaining_Life_Left "
-    "-v 249,raw48,Spare_Blocks_Remaining "
-  },
-  {
-    "StorFly SATA 6Gbps SSDs", 
-    // tested with Virtium StorFly VSFB25CC050G-JUN/0202-000
-    "StorFly VSFBM8C[CI](060|120|240)G-...|"
-    "StorFly VSFBM8C[CI](050|100|200)G-...|"
-    "StorFly VSFBM[48]XI128G-C001", 
+    "StorFly SATA 6Gbps SSDs",
+    // tested with Virtium StorFly VSFBM8CC120G-160
+    "",
     // C - commercial, I industrial
     "0202-000", "",
-    "-v 1,raw24/raw32,Raw_Read_Error_Rate "
-    "-v 5,raw48,Reallocated_Sector_Count "
-    "-v 9,raw24(raw8),Power_On_Hours "
-    "-v 12,raw48,Power_Cycle_Count "
+    //"-v 1,raw24/raw32,Raw_Read_Error_Rate "
+    //"-v 5,raw48,Reallocated_Sector_Count "
+    //"-v 9,raw24(raw8),Power_On_Hours "
+    //"-v 12,raw48,Power_Cycle_Count "
     "-v 160,raw48,Uncorrectable_Count "
     "-v 161,raw48,Spares_Remaining "
     "-v 163,raw48,Initial_Invalid_Blocks "
@@ -878,20 +728,20 @@ const drive_settings builtin_knowndrives[] = {
     "-v 166,raw48,Minimum_Erase_Count "
     "-v 167,raw48,Average_Erase_Count "
     "-v 168,raw48,NAND_Endurance "
-    "-v 177,raw48,Total_Wear_Level_Count "
-    "-v 178,raw48,Runtime_Invalid_Blocks "
-    "-v 181,raw48,Total_Program_Fail "
-    "-v 182,raw48,Total_Erase_Fail "
-    "-v 187,raw48,UECC_Count "
-    "-v 192,raw48,Unsafe_Power_Down "
-    "-v 194,raw48,Temperature "
-    "-v 195,raw48,Hardware_ECC_Recovered "
-    "-v 196,raw48,Reallocation_Event_Count "
-    "-v 198,raw48,Offline_Uncorrectable "
-    "-v 199,raw48,CRC_Error_Count "
-    "-v 232,raw48,Available_Reserve_Space "
-    "-v 241,raw48,Total_LBAs_Written "
-    "-v 242,raw48,Total_LBAs_Read "
+    //"-v 177,raw48,Total_Wear_Level_Count "
+    //"-v 178,raw48,Runtime_Invalid_Blocks "
+    //"-v 181,raw48,Total_Program_Fail "
+    //"-v 182,raw48,Total_Erase_Fail "
+    //"-v 187,raw48,UECC_Count "
+    //"-v 192,raw48,Unsafe_Power_Down "
+    //"-v 194,raw48,Temperature "
+    //"-v 195,raw48,Hardware_ECC_Recovered "
+    //"-v 196,raw48,Reallocation_Event_Count "
+    //"-v 198,raw48,Offline_Uncorrectable "
+    //"-v 199,raw48,CRC_Error_Count "
+    //"-v 232,raw48,Available_Reserve_Space "
+    //"-v 241,raw48,Total_LBAs_Written "
+    //"-v 242,raw48,Total_LBAs_Read "
     "-v 248,raw48,Remaining_Life_Left "
     "-v 249,raw48,Spare_Blocks_Remaining "
   },
