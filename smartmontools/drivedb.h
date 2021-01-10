e5da8f87afa1e8ccc3b87d3be11be14db5906e8e
Christian Franke <christian.franke@t-online.de>
2021-01-10 16:24:36 +0000

    drivedb.h: Add VERSION information to 5.41-7.2 branches (#1424).
    svn-path: branches/RELEASE_7_0_DRIVEDB@5164

--- smartmontools/drivedb.h
+++ smartmontools/drivedb.h
@@ -4,7 +4,7 @@
  * Home page of code is: https://www.smartmontools.org
  *
  * Copyright (C) 2003-11 Philip Williams, Bruce Allen
- * Copyright (C) 2008-20 Christian Franke
+ * Copyright (C) 2008-21 Christian Franke
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
@@ -68,9 +68,9 @@
 /*
 const drive_settings builtin_knowndrives[] = {
  */
-  { "$Id: drivedb.h 5162 2021-01-06 18:42:50Z chrfranke $",
+  { "VERSION: 7.0/5163 2021-01-10 15:52:20 $Id: drivedb.h 5164 2021-01-10 16:24:36Z chrfranke $",
     "-", "-",
-    "This is a dummy entry to hold the SVN-Id of drivedb.h",
+    "Version information",
     ""
   },
   { "DEFAULT",
