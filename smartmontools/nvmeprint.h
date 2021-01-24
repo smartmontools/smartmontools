081cb529542115023913d3d6ad1a67d97fa10910
Christian Franke <christian.franke@t-online.de>
2021-01-24 16:40:20 +0000

    ataprint.h, nvmeprint.h, scsiprint.h: Use C++11 in-class member initializers.
    svn-path: trunk@5183

--- smartmontools/nvmeprint.h
+++ smartmontools/nvmeprint.h
@@ -1,9 +1,9 @@
 /*
  * nvmeprint.h
  *
- * Home page of code is: http://www.smartmontools.org
+ * Home page of code is: https://www.smartmontools.org
  *
- * Copyright (C) 2016 Christian Franke
+ * Copyright (C) 2016-21 Christian Franke
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
@@ -11,30 +11,20 @@
 #ifndef NVMEPRINT_H
 #define NVMEPRINT_H
 
-#define NVMEPRINT_H_CVSID "$Id: nvmeprint.h 4760 2018-08-19 18:45:53Z chrfranke $"
+#define NVMEPRINT_H_CVSID "$Id: nvmeprint.h 5183 2021-01-24 16:40:20Z chrfranke $"
 
 #include "nvmecmds.h"
 
 // options for nvmePrintMain
 struct nvme_print_options
 {
-  bool drive_info;
-  bool drive_capabilities;
-  bool smart_check_status;
-  bool smart_vendor_attrib;
-  unsigned error_log_entries;
-  unsigned char log_page;
-  unsigned log_page_size;
-
-  nvme_print_options()
-    : drive_info(false),
-      drive_capabilities(false),
-      smart_check_status(false),
-      smart_vendor_attrib(false),
-      error_log_entries(0),
-      log_page(0),
-      log_page_size(0)
-    { }
+  bool drive_info = false;
+  bool drive_capabilities = false;
+  bool smart_check_status = false;
+  bool smart_vendor_attrib = false;
+  unsigned error_log_entries = 0;
+  unsigned char log_page = 0;
+  unsigned log_page_size = 0;
 };
 
 int nvmePrintMain(nvme_device * device, const nvme_print_options & options);
