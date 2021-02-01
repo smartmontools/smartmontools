e494f04f3d80db67ce58864bac539bf7098b303a
chrfranke <authors@smartmontools.org>
2021-02-01 17:46:42 +0000

    cppcheck.sh: Add '-c' option.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@5194 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/cppcheck.sh
+++ smartmontools/cppcheck.sh
@@ -4,7 +4,7 @@
 #
 # Home page of code is: https://www.smartmontools.org
 #
-# Copyright (C) 2019-20 Christian Franke
+# Copyright (C) 2019-21 Christian Franke
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
@@ -17,18 +17,20 @@ myname=$0
 
 usage()
 {
-  echo "Usage: $myname [-v|-q] [-jJOBS] [--library=CFG] [--platform=TYPE] [FILE ...]"
+  echo "Usage: $myname [-v|-q] [-c CPPCHECK] [-jJOBS] [--library=CFG] [--platform=TYPE] [FILE ...]"
   exit 1
 }
 
 # Parse options
 jobs=
 v=
+cppcheck="cppcheck"
 library="--library=posix"
 platform="--platform=unix64"
 unused_func=",unusedFunction"
 
 while true; do case $1 in
+  -c) shift; test -n "$1" || usage; cppcheck=$1 ;;
   -j?*) jobs=$1; unused_func= ;;
   -q) v="-q" ;;
   -v) v="-v" ;;
@@ -58,7 +60,7 @@ else
 fi
 
 # Check cppcheck version
-ver=$(cppcheck --version) || exit 1
+ver=$("$cppcheck" --version) || exit 1
 ver=${ver##* }
 case $ver in
   1.85) ;;
@@ -126,7 +128,7 @@ $(for s in $suppress; do echo "  $s \\"; done)
 EOF
 
 # Run cppcheck with swapped stdout<>stderr
-cppcheck \
+"$cppcheck" \
   $v \
   $jobs \
   --enable="$enable" \
