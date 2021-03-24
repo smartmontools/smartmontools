c8e6494bfdf7ea78494c47d33ff3a76187b9f4a4
chrfranke <authors@smartmontools.org>
2021-03-24 16:52:01 +0000

    os_openbsd.cpp: Use correct devicename for autodetection (#1465).
    Patch by Martin Ziemer.
    
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@5213 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_openbsd.cpp
+++ smartmontools/os_openbsd.cpp
@@ -582,17 +582,17 @@ smart_device * openbsd_smart_interface::autodetect_smart_device(const char * nam
   if (str_starts_with(test_name, net_dev_prefix)) {
     test_name += strlen(net_dev_prefix);
     if (!strncmp(net_dev_ata_disk, test_name, strlen(net_dev_ata_disk)))
-      return get_ata_device(test_name, "ata");
+      return get_ata_device(name, "ata");
     if (!strncmp(net_dev_scsi_disk, test_name, strlen(net_dev_scsi_disk))) {
       // XXX Try to detect possible USB->(S)ATA bridge
       // XXX get USB vendor ID, product ID and version from sd(4)/umass(4).
       // XXX check sat device via get_usb_dev_type_by_id().
 
       // No USB bridge found, assume regular SCSI device
-      return get_scsi_device(test_name, "scsi");
+      return get_scsi_device(name, "scsi");
     }
     if (!strncmp(net_dev_scsi_tape, test_name, strlen(net_dev_scsi_tape)))
-      return get_scsi_device(test_name, "scsi");
+      return get_scsi_device(name, "scsi");
   }
   // device type unknown
   return 0;
