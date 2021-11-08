b8034a4438afa90e42fe06b68125edb77dbed2a5
Yannick Hemery <>
2021-11-08 17:11:53 +0000

    scsicmds.cpp: Retry on UNIT ATTENTION when fetching capacity (#1303).
    If a UNIT ATTENTION is raised when READ CAPACITY (16) is called,
    smartctl might wrongly assume that the drive only supports READ
    CAPACITY (10), and return an invalid drive size / block size as a
    result.
    
    Patch by Yannick Hemery.
    
    svn-path: trunk@5244

--- smartmontools/scsicmds.h
+++ smartmontools/scsicmds.h
@@ -19,7 +19,7 @@
 #ifndef SCSICMDS_H_
 #define SCSICMDS_H_
 
-#define SCSICMDS_H_CVSID "$Id: scsicmds.h 5217 2021-05-03 02:31:27Z dpgilbert $\n"
+#define SCSICMDS_H_CVSID "$Id: scsicmds.h 5244 2021-11-08 17:11:53Z chrfranke $\n"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -439,6 +439,8 @@ int scsiSetPowerCondition(scsi_device * device, int power_cond,
 
 int scsiSendDiagnostic(scsi_device * device, int functioncode, uint8_t *pBuf,
                        int bufLen);
+
+bool scsi_pass_through_with_retry(scsi_device * device, scsi_cmnd_io * iop);
 
 int scsiReadDefect10(scsi_device * device, int req_plist, int req_glist,
                      int dl_format, uint8_t *pBuf, int bufLen);
