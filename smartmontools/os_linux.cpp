e12bffe16225a2d53c109c8da6231fa52de1cc3f
Christian Franke <christian.franke@t-online.de>
2022-12-05 15:31:14 +0000

    os_linux.cpp: Build fix for r5420.
    svn-path: trunk@5421

--- smartmontools/os_linux.cpp
+++ smartmontools/os_linux.cpp
@@ -90,7 +90,7 @@
 
 #define ARGUSED(x) ((void)(x))
 
-const char * os_linux_cpp_cvsid = "$Id: os_linux.cpp 5420 2022-12-05 15:01:43Z chrfranke $"
+const char * os_linux_cpp_cvsid = "$Id: os_linux.cpp 5421 2022-12-05 15:31:14Z chrfranke $"
   OS_LINUX_H_CVSID;
 extern unsigned char failuretest_permissive;
 
@@ -1415,7 +1415,7 @@ bool linux_sssraid_device::scsi_pass_through(scsi_cmnd_io *iop)
     char buff[256];
     const int sz = (int)sizeof(buff);
 
-    np = scsi_get_opcode_name(ucp[0]);
+    np = scsi_get_opcode_name(ucp);
     j = snprintf(buff, sz, " [%s: ", np ? np : "<unknown opcode>");
     for (k = 0; k < (int)iop->cmnd_len; ++k)
       j += snprintf(&buff[j], (sz > j ? (sz - j) : 0), "%02x ", ucp[k]);
