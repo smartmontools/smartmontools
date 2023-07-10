38bda640f9b36a69692da061004ed1636630e96a
Christian Franke <christian.franke@t-online.de>
2023-07-10 13:17:30 +0000

    scsiprint.cpp: Remove unused code, remove unused variable (cppcheck 2.11: knownConditionTrueFalse, unusedVariable).
    svn-path: trunk@5495

--- smartmontools/scsiprint.cpp
+++ smartmontools/scsiprint.cpp
@@ -33,7 +33,7 @@
 
 #define GBUF_SIZE 65532
 
-const char * scsiprint_c_cvsid = "$Id: scsiprint.cpp 5485 2023-06-26 12:01:37Z chrfranke $"
+const char * scsiprint_c_cvsid = "$Id: scsiprint.cpp 5495 2023-07-10 13:17:30Z chrfranke $"
                                  SCSIPRINT_H_CVSID;
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
@@ -494,7 +494,6 @@ scsiGetStartStopData(scsi_device * device)
     unsigned char * ucp;
     char b[32];
     const char * q;
-    std::string s;
     static const char * jname = "scsi_start_stop_cycle_counter";
 
     if ((err = scsiLogSense(device, STARTSTOP_CYCLE_COUNTER_LPAGE, 0, gBuf,
@@ -681,15 +680,12 @@ scsiPrintGrownDefectListLen(scsi_device * device, bool prefer12)
                                    1 /* req_glist */,
                                    4 /* format: bytes from index */, gBuf, 4);
             if (2 == err) { /* command not supported */
-                if (err) {
-                    if (scsi_debugmode > 0) {
-                        print_on();
-                        pout("%s (10) Failed: %s\n", hname, scsiErrString(err));
-                        print_off();
-                    }
-                    return;
-                } else
-                    got_rd12 = false;
+                if (scsi_debugmode > 0) {
+                    print_on();
+                    pout("%s (10) Failed: %s\n", hname, scsiErrString(err));
+                    print_off();
+                }
+                return;
             } else if (101 == err)    /* Defect list not found, leave quietly */
                 return;
             else {
