95c6707db9cc63cd6877f4dfae706db1e43b2d66
chrfranke <authors@smartmontools.org>
2023-07-10 16:18:14 +0000

    os_win32/syslog_win32.cpp: Replace tabs.
    git-svn-id: http://svn.code.sf.net/p/smartmontools/code/trunk@5497 4ea69e1a-61f1-4043-bf83-b5c94c648137

--- smartmontools/os_win32/syslog_win32.cpp
+++ smartmontools/os_win32/syslog_win32.cpp
@@ -48,19 +48,19 @@ const char *syslog_win32_cpp_cvsid = "$Id$"
 // Redirect event log to stdout for testing
 
 static BOOL Test_ReportEventA(HANDLE h, WORD type, WORD cat, DWORD id, PSID usid,
-						WORD nstrings, WORD datasize, LPCTSTR * strings, LPVOID data)
+            WORD nstrings, WORD datasize, LPCTSTR * strings, LPVOID data)
 {
-	int i;
-	printf("%u %lu:%s", type, id, nstrings != 1?"\n":"");
-	for (i = 0; i < nstrings; i++)
-		printf(" \"%s\"\n", strings[i]);
-	fflush(stdout);
-	return TRUE;
+  int i;
+  printf("%u %lu:%s", type, id, nstrings != 1?"\n":"");
+  for (i = 0; i < nstrings; i++)
+    printf(" \"%s\"\n", strings[i]);
+  fflush(stdout);
+  return TRUE;
 }
 
 HANDLE Test_RegisterEventSourceA(LPCTSTR server, LPCTSTR source)
 {
-	return (HANDLE)42;
+  return (HANDLE)42;
 }
 
 #define ReportEventA Test_ReportEventA
@@ -99,17 +99,17 @@ static HANDLE evt_wait_in, evt_wait_out;
 
 static WORD pri2evtype(int priority)
 {
-	switch (priority) {
-		default:
-		case LOG_EMERG: case LOG_ALERT:
-		case LOG_CRIT:  case LOG_ERR:
-			return EVENTLOG_ERROR_TYPE;
-		case LOG_WARNING:
-			return EVENTLOG_WARNING_TYPE;
-		case LOG_NOTICE: case LOG_INFO:
-		case LOG_DEBUG:
-			return EVENTLOG_INFORMATION_TYPE;
-	}
+  switch (priority) {
+    default:
+    case LOG_EMERG: case LOG_ALERT:
+    case LOG_CRIT:  case LOG_ERR:
+      return EVENTLOG_ERROR_TYPE;
+    case LOG_WARNING:
+      return EVENTLOG_WARNING_TYPE;
+    case LOG_NOTICE: case LOG_INFO:
+    case LOG_DEBUG:
+      return EVENTLOG_INFORMATION_TYPE;
+  }
 }
 
 
@@ -117,17 +117,17 @@ static WORD pri2evtype(int priority)
 
 static const char * pri2text(int priority)
 {
-	switch (priority) {
-		case LOG_EMERG:   return "EMERG";
-		case LOG_ALERT:   return "ALERT";
-		case LOG_CRIT:    return "CRIT";
-		default:
-		case LOG_ERR:     return "ERROR";
-		case LOG_WARNING: return "Warn";
-		case LOG_NOTICE:  return "Note";
-		case LOG_INFO:    return "Info";
-		case LOG_DEBUG:   return "Debug";
-	}
+  switch (priority) {
+    case LOG_EMERG:   return "EMERG";
+    case LOG_ALERT:   return "ALERT";
+    case LOG_CRIT:    return "CRIT";
+    default:
+    case LOG_ERR:     return "ERROR";
+    case LOG_WARNING: return "Warn";
+    case LOG_NOTICE:  return "Note";
+    case LOG_INFO:    return "Info";
+    case LOG_DEBUG:   return "Debug";
+  }
 }
 
 
@@ -135,31 +135,31 @@ static const char * pri2text(int priority)
 
 static void report_events(int cnt)
 {
-	const char * msgs[3+MAXLINES];
+  const char * msgs[3+MAXLINES];
 
-	int i, pri;
-	if (cnt <= 0)
-		return;
-	if (cnt > MAXLINES)
-		cnt = MAXLINES;
+  int i, pri;
+  if (cnt <= 0)
+    return;
+  if (cnt > MAXLINES)
+    cnt = MAXLINES;
 
-	pri = evt_priorities[evt_index_out];
+  pri = evt_priorities[evt_index_out];
 
-	msgs[0] = sl_ident;
-	msgs[1] = sl_pidstr;
-	msgs[2] = pri2text(pri);
-	for (i = 0; i < cnt; i++) {
-		//assert(evt_priorities[evt_index_out] == pri);
-		msgs[3+i] = evt_lines[evt_index_out];
-		if (++evt_index_out >= MAXLINES)
-			evt_index_out = 0;
-	}
-	ReportEventA(sl_hevtsrc,
-		pri2evtype(pri), // type
-		0, MSG_SYSLOG+cnt,    // category, message id
-		NULL,                 // no security id
-		(WORD)(3+cnt), 0,     // 3+cnt strings, ...
-		msgs, NULL);          // ...          , no data
+  msgs[0] = sl_ident;
+  msgs[1] = sl_pidstr;
+  msgs[2] = pri2text(pri);
+  for (i = 0; i < cnt; i++) {
+    //assert(evt_priorities[evt_index_out] == pri);
+    msgs[3+i] = evt_lines[evt_index_out];
+    if (++evt_index_out >= MAXLINES)
+      evt_index_out = 0;
+  }
+  ReportEventA(sl_hevtsrc,
+    pri2evtype(pri),    // type
+    0, MSG_SYSLOG+cnt,  // category, message id
+    NULL,               // no security id
+    (WORD)(3+cnt), 0,   // 3+cnt strings, ...
+    msgs, NULL);        // ...          , no data
 }
 
 
@@ -167,81 +167,81 @@ static void report_events(int cnt)
 
 static ULONG WINAPI event_logger_thread(LPVOID arg)
 {
-	int cnt;
-	ARGUSED(arg);
+  int cnt;
+  ARGUSED(arg);
 
-	cnt = 0;
-	for (;;) {
-		// Wait for first line ...
-		int prior, i, rest;
-		if (cnt == 0) {
-			if (WaitForSingleObject(evt_wait_out, (evt_timeout? INFINITE : 0)) != WAIT_OBJECT_0)
-				break;
-			cnt = 1;
-		}
+  cnt = 0;
+  for (;;) {
+    // Wait for first line ...
+    int prior, i, rest;
+    if (cnt == 0) {
+      if (WaitForSingleObject(evt_wait_out, (evt_timeout? INFINITE : 0)) != WAIT_OBJECT_0)
+        break;
+      cnt = 1;
+    }
 
-		// ... wait some time for more lines with same prior
-		i = evt_index_out;
-		prior = evt_priorities[i];
-		rest = 0;
-		while (cnt < MAXLINES) {
-			long timeout =
-				evt_timeout * ((1000L * (MAXLINES-cnt+1))/MAXLINES);
-			if (WaitForSingleObject(evt_wait_out, timeout) != WAIT_OBJECT_0)
-				break;
-			if (++i >= MAXLINES)
-				i = 0;
-			if (evt_priorities[i] != prior) {
-				rest = 1;
-				break;
-			}
-			cnt++;
-		}
+    // ... wait some time for more lines with same prior
+    i = evt_index_out;
+    prior = evt_priorities[i];
+    rest = 0;
+    while (cnt < MAXLINES) {
+      long timeout =
+        evt_timeout * ((1000L * (MAXLINES-cnt+1))/MAXLINES);
+      if (WaitForSingleObject(evt_wait_out, timeout) != WAIT_OBJECT_0)
+        break;
+      if (++i >= MAXLINES)
+        i = 0;
+      if (evt_priorities[i] != prior) {
+        rest = 1;
+        break;
+      }
+      cnt++;
+    }
 
-		// Output all in one event log entry
-		report_events(cnt);
+    // Output all in one event log entry
+    report_events(cnt);
 
-		// Signal space
-		if (!ReleaseSemaphore(evt_wait_in, cnt, NULL))
-			break;
-		cnt = rest;
-	}
-	return 0;
+    // Signal space
+    if (!ReleaseSemaphore(evt_wait_in, cnt, NULL))
+      break;
+    cnt = rest;
+  }
+  return 0;
 }
 
 
 static void on_exit_event_logger(void)
 {
-	// Output lines immediate if exiting
-	evt_timeout = 0; 
-	// Wait for thread to finish
-	WaitForSingleObject(evt_hthread, 1000L);
-	CloseHandle(evt_hthread);
+  // Output lines immediate if exiting
+  evt_timeout = 0;
+  // Wait for thread to finish
+  WaitForSingleObject(evt_hthread, 1000L);
+  CloseHandle(evt_hthread);
 #if 0
-	if (sl_hevtsrc) {
-		DeregisterEventSource(sl_hevtsrc); sl_hevtsrc = 0;
-	}
+  if (sl_hevtsrc) {
+    DeregisterEventSource(sl_hevtsrc); sl_hevtsrc = 0;
+  }
 #else
-	// Leave event message source open to prevent losing messages during shutdown
+  // Leave event message source open to prevent losing messages during shutdown
 #endif
 }
 
 
 static int start_event_logger()
 {
-	DWORD tid;
-	evt_timeout = 1;
-	if (   !(evt_wait_in  = CreateSemaphore(NULL,  MAXLINES, MAXLINES, NULL))
-		|| !(evt_wait_out = CreateSemaphore(NULL,         0, MAXLINES, NULL))) {
-		fprintf(stderr,"CreateSemaphore failed, Error=%ld\n", GetLastError());
-		return -1;
-	}
-	if (!(evt_hthread = CreateThread(NULL, 0, event_logger_thread, NULL, 0, &tid))) {
-		fprintf(stderr,"CreateThread failed, Error=%ld\n", GetLastError());
-		return -1;
-	}
-	atexit(on_exit_event_logger);
-	return 0;
+  DWORD tid;
+  evt_timeout = 1;
+  if (   !(evt_wait_in  = CreateSemaphore(NULL,  MAXLINES, MAXLINES, NULL))
+    || !(evt_wait_out = CreateSemaphore(NULL,         0, MAXLINES, NULL))) {
+    fprintf(stderr,"CreateSemaphore failed, Error=%ld\n", GetLastError());
+    return -1;
+  }
+  if (!(evt_hthread = CreateThread(NULL, 0, event_logger_thread, NULL, 0, &tid))) {
+    fprintf(stderr,"CreateThread failed, Error=%ld\n", GetLastError());
+    return -1;
+  }
+  atexit(on_exit_event_logger);
+  return 0;
 }
 
 
@@ -249,39 +249,39 @@ static int start_event_logger()
 
 static void write_event_log(int priority, const char * lines)
 {
-	int cnt = 0;
-	int i;
-	for (i = 0; lines[i]; i++) {
-		int len = 0;
-		while (lines[i+len] && lines[i+len] != '\n')
-			len++;
-			;
-		if (len > 0) {
-			// Wait for space
-			if (WaitForSingleObject(evt_wait_in, INFINITE) != WAIT_OBJECT_0)
-				return;
-			// Copy line
-			evt_priorities[evt_index_in] = priority;
-			memcpy(evt_lines[evt_index_in], lines+i, (len < LINELEN ? len : LINELEN));
-			if (len < LINELEN)
-				evt_lines[evt_index_in][len] = 0;
-			if (++evt_index_in >= MAXLINES)
-				evt_index_in = 0;
-			// Signal avail if ring buffer full
-			if (++cnt >= MAXLINES) {
-				ReleaseSemaphore(evt_wait_out, cnt, NULL);
-				cnt = 0;
-			}
-			i += len;
-		}
-		if (!lines[i])
-			break;
-	}
+  int cnt = 0;
+  int i;
+  for (i = 0; lines[i]; i++) {
+    int len = 0;
+    while (lines[i+len] && lines[i+len] != '\n')
+      len++;
+      ;
+    if (len > 0) {
+      // Wait for space
+      if (WaitForSingleObject(evt_wait_in, INFINITE) != WAIT_OBJECT_0)
+        return;
+      // Copy line
+      evt_priorities[evt_index_in] = priority;
+      memcpy(evt_lines[evt_index_in], lines+i, (len < LINELEN ? len : LINELEN));
+      if (len < LINELEN)
+        evt_lines[evt_index_in][len] = 0;
+      if (++evt_index_in >= MAXLINES)
+        evt_index_in = 0;
+      // Signal avail if ring buffer full
+      if (++cnt >= MAXLINES) {
+        ReleaseSemaphore(evt_wait_out, cnt, NULL);
+        cnt = 0;
+      }
+      i += len;
+    }
+    if (!lines[i])
+      break;
+  }
 
-	// Signal avail
-	if (cnt > 0)
-		ReleaseSemaphore(evt_wait_out, cnt, NULL);
-	Sleep(1);
+  // Signal avail
+  if (cnt > 0)
+    ReleaseSemaphore(evt_wait_out, cnt, NULL);
+  Sleep(1);
 }
 
 
@@ -289,67 +289,67 @@ static void write_event_log(int priority, const char * lines)
 
 static void write_logfile(FILE * f, int priority, const char * lines)
 {
-	char stamp[32];
-	time_t now = time(0);
-	struct tm tmbuf;
-	if (!(   !localtime_s(&tmbuf, &now)
-	      && strftime(stamp, sizeof(stamp), "%Y-%m-%d %H:%M:%S", &tmbuf))) {
-		stamp[0] = '?'; stamp[1] = 0;
-	}
+  char stamp[32];
+  time_t now = time(0);
+  struct tm tmbuf;
+  if (!(   !localtime_s(&tmbuf, &now)
+        && strftime(stamp, sizeof(stamp), "%Y-%m-%d %H:%M:%S", &tmbuf))) {
+    stamp[0] = '?'; stamp[1] = 0;
+  }
 
-	for (int i = 0; lines[i]; i++) {
-		int len = 0;
-		while (lines[i+len] && lines[i+len] != '\n')
-			len++;
-		if (len > 0) {
-			fprintf(f, "%s %s[%s]: %-5s: ",
-				stamp, sl_ident, sl_pidstr, pri2text(priority));
-			fwrite(lines+i, len, 1, f);
-			fputc('\n', f);
-			i += len;
-		}
-		if (!lines[i])
-			break;
-	}
+  for (int i = 0; lines[i]; i++) {
+    int len = 0;
+    while (lines[i+len] && lines[i+len] != '\n')
+      len++;
+    if (len > 0) {
+      fprintf(f, "%s %s[%s]: %-5s: ",
+        stamp, sl_ident, sl_pidstr, pri2text(priority));
+      fwrite(lines+i, len, 1, f);
+      fputc('\n', f);
+      i += len;
+    }
+    if (!lines[i])
+      break;
+  }
 }
 
 
 void openlog(const char *ident, int logopt, int facility)
 {
-	int pid;
-	if (sl_logpath[0] || sl_logfile || sl_hevtsrc)
-		return; // Already open
+  int pid;
+  if (sl_logpath[0] || sl_logfile || sl_hevtsrc)
+    return; // Already open
 
-	strncpy(sl_ident, ident, sizeof(sl_ident)-1);
-	// logopt==LOG_PID assumed
-	ARGUSED(logopt);
-	pid = getpid();
-	if (snprintf(sl_pidstr, sizeof(sl_pidstr)-1, (pid >= 0 ? "%d" : "0x%X"), pid) < 0)
-		strcpy(sl_pidstr,"?");
+  strncpy(sl_ident, ident, sizeof(sl_ident)-1);
+  // logopt==LOG_PID assumed
+  ARGUSED(logopt);
+  pid = getpid();
+  if (snprintf(sl_pidstr, sizeof(sl_pidstr)-1, (pid >= 0 ? "%d" : "0x%X"), pid) < 0)
+    strcpy(sl_pidstr,"?");
 
-	if (facility == LOG_LOCAL0) // "ident.log"
-		strcat(strcpy(sl_logpath, sl_ident), ".log");
-	else if (facility == LOG_LOCAL1) // stdout
-		sl_logfile = stdout;
-	else if (facility == LOG_LOCAL2) // stderr
-		sl_logfile = stderr;
-	else if (LOG_LOCAL2 < facility && facility <= LOG_LOCAL7) { // "ident[1-5].log"
-		snprintf(sl_logpath, sizeof(sl_logpath)-1, "%s%d.log",
-			sl_ident, LOG_FAC(facility)-LOG_FAC(LOG_LOCAL2));
-	}
-	else // Assume LOG_DAEMON, use event log if possible, else "ident.log"
-	if (!(sl_hevtsrc = RegisterEventSourceA(NULL/*localhost*/, sl_ident))) {
-		// Cannot open => Use logfile
-		long err = GetLastError();
-		strcat(strcpy(sl_logpath, sl_ident), ".log");
-		fprintf(stderr, "%s: Cannot register event source (Error=%ld), writing to %s\n",
-			sl_ident, err, sl_logpath);
-	}
-	else {
-		// Start event log thread
-		start_event_logger();
-	}
-	//assert(sl_logpath[0] || sl_logfile || sl_hevtsrc);
+  if (facility == LOG_LOCAL0) // "ident.log"
+    strcat(strcpy(sl_logpath, sl_ident), ".log");
+  else if (facility == LOG_LOCAL1) // stdout
+    sl_logfile = stdout;
+  else if (facility == LOG_LOCAL2) // stderr
+    sl_logfile = stderr;
+  else if (LOG_LOCAL2 < facility && facility <= LOG_LOCAL7) { // "ident[1-5].log"
+    snprintf(sl_logpath, sizeof(sl_logpath)-1, "%s%d.log",
+      sl_ident, LOG_FAC(facility)-LOG_FAC(LOG_LOCAL2));
+  }
+  else // Assume LOG_DAEMON, use event log if possible, else "ident.log"
+  if (!(sl_hevtsrc = RegisterEventSourceA(NULL/*localhost*/, sl_ident))) {
+    // Cannot open => Use logfile
+    long err = GetLastError();
+    strcat(strcpy(sl_logpath, sl_ident), ".log");
+    fprintf(stderr, "%s: Cannot register event source (Error=%ld), writing to %s\n",
+      sl_ident, err, sl_logpath);
+  }
+  else {
+    // Start event log thread
+    start_event_logger();
+  }
+  //assert(sl_logpath[0] || sl_logfile || sl_hevtsrc);
 
 }
 
@@ -361,33 +361,33 @@ void closelog()
 
 void vsyslog(int priority, const char * message, va_list args)
 {
-	char buffer[1000];
+  char buffer[1000];
 
-	// Translation of %m to error text not supported yet
-	if (strstr(message, "%m"))
-		message = "Internal error: \"%%m\" in log message";
+  // Translation of %m to error text not supported yet
+  if (strstr(message, "%m"))
+    message = "Internal error: \"%%m\" in log message";
 
-	// Format message
-	if (vsnprintf(buffer, sizeof(buffer)-1, message, args) < 0)
-		strcpy(buffer, "Internal Error: buffer overflow");
+  // Format message
+  if (vsnprintf(buffer, sizeof(buffer)-1, message, args) < 0)
+    strcpy(buffer, "Internal Error: buffer overflow");
 
-	if (sl_hevtsrc) {
-		// Write to event log
-		write_event_log(priority, buffer);
-	}
-	else if (sl_logfile) {
-		// Write to stdout/err
-		write_logfile(sl_logfile, priority, buffer);
-		fflush(sl_logfile);
-	}
-	else if (sl_logpath[0]) {
-		// Append to logfile
-		FILE * f;
-		if (!(f = fopen(sl_logpath, "a")))
-			return;
-		write_logfile(f, priority, buffer);
-		fclose(f);
-	}
+  if (sl_hevtsrc) {
+    // Write to event log
+    write_event_log(priority, buffer);
+  }
+  else if (sl_logfile) {
+    // Write to stdout/err
+    write_logfile(sl_logfile, priority, buffer);
+    fflush(sl_logfile);
+  }
+  else if (sl_logpath[0]) {
+    // Append to logfile
+    FILE * f;
+    if (!(f = fopen(sl_logpath, "a")))
+      return;
+    write_logfile(f, priority, buffer);
+    fclose(f);
+  }
 }
 
 
@@ -396,28 +396,28 @@ void vsyslog(int priority, const char * message, va_list args)
 
 void syslog(int priority, const char *message, ...)
 {
-	va_list args;
-	va_start(args, message);
-	vsyslog(priority, message, args);
-	va_end(args);
+  va_list args;
+  va_start(args, message);
+  vsyslog(priority, message, args);
+  va_end(args);
 }
 
 int main(int argc, char* argv[])
 {
-	int i;
-	openlog(argc < 2 ? "test" : argv[1], LOG_PID, (argc < 3 ? LOG_DAEMON : LOG_LOCAL1));
-	syslog(LOG_INFO,    "Info\n");
-	syslog(LOG_WARNING, "Warning %d\n\n", 42);
-	syslog(LOG_ERR,     "Error %s", "Fatal");
-	for (i = 0; i < 100; i++) {
-		char buf[LINELEN];
-		if (i % 13 == 0)
-			Sleep(1000L);
-		sprintf(buf, "Log Line %d\n", i);
-		syslog((i % 17) ? LOG_INFO : LOG_ERR, buf);
-	}
-	closelog();
-	return 0;
+  int i;
+  openlog(argc < 2 ? "test" : argv[1], LOG_PID, (argc < 3 ? LOG_DAEMON : LOG_LOCAL1));
+  syslog(LOG_INFO,    "Info\n");
+  syslog(LOG_WARNING, "Warning %d\n\n", 42);
+  syslog(LOG_ERR,     "Error %s", "Fatal");
+  for (i = 0; i < 100; i++) {
+    char buf[LINELEN];
+    if (i % 13 == 0)
+      Sleep(1000L);
+    sprintf(buf, "Log Line %d\n", i);
+    syslog((i % 17) ? LOG_INFO : LOG_ERR, buf);
+  }
+  closelog();
+  return 0;
 }
 
 #endif
