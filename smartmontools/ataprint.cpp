b45dd4d0b0160c83a73a07f461c4426ff0d7d422
Christian Franke <christian.franke@t-online.de>
2025-02-15 15:51:06 +0000

    ataprint.cpp: Add support for '-l farm -T permissive'.
    Overrides the FARM support check for rebranded Seagate drives
    (see GH issues/320).
    
    svn-path: trunk@5662

--- smartmontools/ataprint.cpp
+++ smartmontools/ataprint.cpp
@@ -33,7 +33,7 @@
 #include "farmcmds.h"
 #include "farmprint.h"
 
-const char * ataprint_cpp_cvsid = "$Id: ataprint.cpp 5658 2025-02-02 17:56:14Z chrfranke $"
+const char * ataprint_cpp_cvsid = "$Id: ataprint.cpp 5662 2025-02-15 15:51:06Z chrfranke $"
                                   ATAPRINT_H_CVSID;
 
 
@@ -4553,7 +4553,7 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
   if (options.farm_log || options.farm_log_suggest) {
     bool farm_supported = true;
     // Check if drive is a Seagate drive
-    if (ataIsSeagate(drive, dbentry)) {
+    if (ataIsSeagate(drive, dbentry) || (options.farm_log && is_permissive())) {
       unsigned nsectors = GetNumLogSectors(gplogdir, 0xA6, true);
       // Check if the Seagate drive is one that supports FARM
       if (!nsectors) {
@@ -4578,9 +4578,9 @@ int ataPrintMain (ata_device * device, const ata_print_options & options)
         }
       }
     } else {
-      if (options.farm_log) {
-        jout("FARM log (GP Log 0xa6) not supported for non-Seagate drives\n\n");
-      }
+      if (options.farm_log)
+        jout("FARM log (GP Log 0xa6) not supported for non-Seagate drives\n"
+             "(override with '-T permissive' option)\n\n");
       farm_supported = false;
     }
     jglb["seagate_farm_log"]["supported"] = farm_supported;
